6ab71144752bf6e4ee0d79194d1eb507e229fcf8
Claudio <claudio@tracking.exposed>
2018-07-31 22:55:46 +0200

    build.sh

---
+++ backend/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+rm -rf ./dist
+
+V=dist/js/ 
+mkdir -p dist/css && 
+mkdir -p dist/js/local 
+mkdir -p dist/images 
+mkdir -p $V 
+
+echo "User-agent: *\nAllow: /\n" >> dist/robots.txt
+
+node_modules/.bin/stylus styles/index.styl -o dist/css
+
+cp styles/favicon.ico dist/
+cp -r ../icons/* dist/images
+# cp -r sections/images/* dist/images
+# cp -r sections/webscripts/* dist/js/local/ 
+cp -r node_modules/bootstrap/dist/* dist/
+
+cp node_modules/bootstrap/js/collapse.js $V
+cp node_modules/jquery/dist/jquery.js $V 
+cp node_modules/moment/min/moment.min.js $V 
+cp node_modules/lodash/lodash.min.js $V 
+
