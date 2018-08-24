0deed8af06e66a7e1a502c8e35b7c2bd72510fb7
Claudio <claudio@tracking.exposed>
2018-08-24 18:08:17 +0200

    import still in progress

---
+++ src/handlers/sync.js
@@ -0,0 +1,47 @@
+import config from '../config';
+import { getTimeISO8601 } from '../utils';
+const bo = chrome || browser;
+
+const INTERVAL = config.FLUSH_INTERVAL;
+
+var state = {
+    incremental: 0,
+    videos: []
+};
+
+function handleVideo (type, e) {
+    console.log("handleVideo");
+    console.log(e);
+
+    state.videos.push({
+        element: e.element,
+        tagId: config.settings.tagId,
+        href: e.href,
+        incremental: state.incremental,
+        clientTime: getTimeISO8601()
+    });
+    state.incremental++;
+}
+
+function sync (hub) {
+    if (state.videos.length) {
+        console.log("found some video to flush");
+        // Send timelines to the page handling the communication with the API.
+        // This might be refactored using something compatible to the HUB architecture.
+        bo.runtime.sendMessage({ type: 'sync', payload: state.videos, userId: config.userId },
+                                   (response) => hub.event('syncResponse', response));
+
+        state.videos = [];
+    } else console.log("queue is empty");
+}
+
+export function register (hub) {
+    hub.register('newVideo', handleVideo);
+    hub.register('windowUnload', sync.bind(null, hub));
+    window.setInterval(sync.bind(null, hub), INTERVAL);
+}
+
+
+
+
+
