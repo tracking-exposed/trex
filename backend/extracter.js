c60c84d5bcf3065551931a436993cf90e97beccf
Claudio TREX <claudio@tracking.exposed>
2019-04-17 13:32:30 +0200

    added extractor

---
+++ backend/extracter.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const Promise = require('bluebird');
+const _ = require('lodash');
+const moment = require('moment');
+const debug = require('debug')('extracter');
+const nconf = require('nconf');
+
+nconf.file({ file: 'config/settings.json' });
+
+const mongo = require('./lib/mongo');
+const utils = require('./lib/utils');
+
+
+return mongo
+	.read('metadata', {})
+        .map(function(p) {
+		let related = _.map(p.related, function(e) {
+			return _.pick(e, ['id', 'videoId', 'title', 'source', 'vizstr', 'suggestionOrder']);
+		});
+		let clean = {
+			watched: p.title,
+			since: p.publicationString,
+			credited: p.authorName,
+			channel: p.authorSource
+		};
+		clean.watchId = p.videoId.replace(/\&.*/, '');
+		return _.map(related, function(eachone) {
+			return _.extend(eachone, clean);
+		});
+	})
+	.then(_.flatten)
+	.then(function(all) {
+		const fs = Promise.promisifyAll(require('fs'));
+		const destf = 'ytmetamerged.json';
+		console.log("dumping ", _.size(all), "objects in all.json");
+		return fs.writeFileAsync(destf, JSON.stringify(all, undefined, 1));
+	});
+
