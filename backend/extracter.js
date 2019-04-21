83a8c3e433fc3bd9c8be9964a3bdc9552107f5a4
Claudio Agosti <claudio@tracking.exposed>
2019-04-21 02:42:06 +0200

    fixed the two missing fields in parser

--- backend/extracter.js
+++ backend/extracter.js
@@ -10,29 +10,27 @@ nconf.file({ file: 'config/settings.json' });
 const mongo = require('./lib/mongo');
 const utils = require('./lib/utils');
 
-
 return mongo
-	.read('metadata', {})
+    .read('metadata', {})
         .map(function(p) {
-		let related = _.map(p.related, function(e) {
-			return _.pick(e, ['id', 'videoId', 'title', 'source', 'vizstr', 'suggestionOrder']);
-		});
-		let clean = {
-			watched: p.title,
-			since: p.publicationString,
-			credited: p.authorName,
-			channel: p.authorSource
-		};
-		clean.watchId = p.videoId.replace(/\&.*/, '');
-		return _.map(related, function(eachone) {
-			return _.extend(eachone, clean);
-		});
-	})
-	.then(_.flatten)
-	.then(function(all) {
-		const fs = Promise.promisifyAll(require('fs'));
-		const destf = 'ytmetamerged.json';
-		console.log("dumping ", _.size(all), "objects in all.json");
-		return fs.writeFileAsync(destf, JSON.stringify(all, undefined, 1));
-	});
-
+        let related = _.map(p.related, function(e) {
+            return _.pick(e, ['id', 'videoId', 'title', 'source', 'vizstr', 'suggestionOrder']);
+        });
+        let clean = {
+            watched: p.title,
+            since: p.publicationString,
+            credited: p.authorName,
+            channel: p.authorSource
+        };
+        clean.watchId = p.videoId.replace(/\&.*/, '');
+        return _.map(related, function(eachone) {
+            return _.extend(eachone, clean);
+        });
+    })
+    .then(_.flatten)
+    .then(function(all) {
+        const fs = Promise.promisifyAll(require('fs'));
+        const destf = 'ytmetamerged.json';
+        console.log("dumping ", _.size(all), "objects in all.json");
+        return fs.writeFileAsync(destf, JSON.stringify(all, undefined, 1));
+    });
