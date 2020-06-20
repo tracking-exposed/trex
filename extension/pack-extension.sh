9c7de98878681cc24c42e25a360c6c2bdefa4fb7
Claudio <claudio@tracking.exposed>
2020-06-20 02:16:04 +0200

    fixed a line that would have broken reproducible building

--- extension/pack-extension.sh
+++ extension/pack-extension.sh
@@ -6,7 +6,7 @@ NODE_ENV=production node_modules/.bin/webpack -p
 echo "Manually removing 'localhost:9000' from the manifest.json"
 # This is savage.
 grep -v localhost manifest.json | grep -v 127\.0 > ./dist/manifest.json
-cp ../backend/parsers/longlabel.js src/
+echo "Developer, remind to check if ../backend/parsers/longlabel.js and src/longlabel.js differs!"
 
 #V=`git tag -l | head -1`
 #sed -es'/BUILD_VERSION/'$V'/' src/popup/popup.html  > dist/popup.html
