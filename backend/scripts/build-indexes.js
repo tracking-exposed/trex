90ba4394d1b8f735e0bbd9cc039bf01b6623495d
Claudio <claudio@tracking.exposed>
2020-09-24 15:07:07 +0200

    added index for 'errors'

--- backend/scripts/build-indexes.js
+++ backend/scripts/build-indexes.js
@@ -1,10 +1,10 @@
-ret = db.metadata.createIndex({id: 1}, {unique: true }); checkret('metadata id', ret);
-ret = db.metadata.createIndex({videoId: 1}); checkret('metadata videoId', ret);
-ret = db.metadata.createIndex({type: 1}); checkret('metadata type', ret);
-ret = db.metadata.createIndex({"related.videoId": 1}); checkret('metadata related.videoId', ret);
-ret = db.metadata.createIndex({"selected.videoId": 1}); checkret('metadata selected.videoId', ret);
-ret = db.metadata.createIndex({authorName: 1}); checkret('metadata authorName', ret);
-ret = db.metadata.createIndex({savingTime: -1}); checkret('metadata savingTime', ret);
+ret = db.metadata.createIndex({ id: 1 }, { unique: true }); checkret('metadata id', ret);
+ret = db.metadata.createIndex({ videoId: 1 }); checkret('metadata videoId', ret);
+ret = db.metadata.createIndex({ type: 1 }); checkret('metadata type', ret);
+ret = db.metadata.createIndex({ "related.videoId": 1 }); checkret('metadata related.videoId', ret);
+ret = db.metadata.createIndex({ "selected.videoId": 1 }); checkret('metadata selected.videoId', ret);
+ret = db.metadata.createIndex({ authorName: 1 }); checkret('metadata authorName', ret);
+ret = db.metadata.createIndex({ savingTime: -1 }); checkret('metadata savingTime', ret);
 
 ret = db.supporters.createIndex({ publicKey: 1 }, { unique: true }); checkret('supporters publicKey:', ret);
 ret = db.supporters.createIndex({ lastActivity: 1 }); checkret('supporters lastActivity:', ret);
@@ -13,19 +13,22 @@ ret = db.groups.createIndex({ id: 1 }, { unique: true }); checkret('groups id', 
 ret = db.groups.createIndex({ name: 1 }, { unique: true }); checkret('groups name', ret);
 
 /* metadataId is not used to address content, if not when 'precise' parser is invoked */
-ret = db.htmls.createIndex({ id: 1 }, { unique: true} ); checkret('htmls id', ret);
+ret = db.htmls.createIndex({ id: 1 }, { unique: true }); checkret('htmls id', ret);
 ret = db.htmls.createIndex({ savingTime: -1 }); checkret('htmls savingTime', ret);
 ret = db.htmls.createIndex({ processed: 1 }); checkret('htmls processed', ret);
 ret = db.htmls.createIndex({ metadataId: 1 }); checkret('htmls metadataId', ret);
-ret = db.htmls.createIndex({ isSearch: 1 }); checkret('htmls isSearch', ret);
+ret = db.htmls.createIndex({ type: 1 }); checkret('htmls type', ret);
 
 ret = db.labels.createIndex({ metadataId: 1 }); checkret('labels processed', ret);
-ret = db.labels.createIndex({ savingTime: -1}, { expireAfterSeconds: 7 * 24 * 3600 } ); checkret('labels savingTime expiring', ret);
+ret = db.labels.createIndex({ savingTime: -1}, { expireAfterSeconds: 7 * 24 * 3600 }); checkret('labels savingTime expiring', ret);
 ret = db.labels.createIndex({ selectorName: 1}); checkret('labels expiring', ret);
 
 ret = db.searches.createIndex({ metadataId: 1 }); checkret('searches metadataId', ret);
-ret = db.searches.createIndex({ savingTime: -1}); checkret('searches savingTime', ret);
-ret = db.searches.createIndex({ id: 1}, { unique: true} ); checkret('searches id', ret);
+ret = db.searches.createIndex({ savingTime: -1 }); checkret('searches savingTime', ret);
+ret = db.searches.createIndex({ id: 1 }, { unique: true }); checkret('searches id', ret);
+
+ret = db.errors.createIndex({ id: 1 }, { unique: true }); checkret('errors id', ret);
+ret = db.errors.createIndex({ when: -1 }); checkret('errors when', ret);
 
 function checkret(info, retval) {
     retval.info = info;
