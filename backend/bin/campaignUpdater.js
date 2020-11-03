47f3d87ed1c3db8d248cdbb238aea451e2e9fdc2
Claudio <claudio@tracking.exposed>
2020-11-03 10:57:02 +0100

    simplified the remote/local usage

--- backend/bin/campaignUpdater.js
+++ backend/bin/campaignUpdater.js
@@ -8,12 +8,12 @@ var nconf = require('nconf');
 nconf.argv().env();
 
 if(!nconf.get('key'))
-    return console.log("--key required (other allowed params --source <jsonFile> default config/campaigns.json, and --dest <host>, default localhost:9000)");
+    return console.log("--key required (other allowed params --source <jsonFile> default config/campaigns.json, and --remote (bool), default localhost:9000)");
 
 async function main() {
 
     const sourcefile = nconf.get('source') || 'config/campaigns.json';
-    const dest = nconf.get('dest') || 'http://localhost:9000';
+    const dest = (nconf.get('remote')) ? 'https://youtube.tracking.exposed' : 'http://localhost:9000';
     const destUrl = `${dest}/api/v2/campaigns/${nconf.get('key')}/`;
 
     const content = JSON.parse(fs.readFileSync(sourcefile));
