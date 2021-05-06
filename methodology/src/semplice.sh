418c461dcc90300a049535f6b45f564e64f60b45
Claudio <claudio@syrianarchive.org>
2021-05-06 19:16:16 +0200

    fixed russian band n2

--- methodology/src/semplice.sh
+++ methodology/src/semplice.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
-source=json/fish-control-1.json DEBUG=*,-puppeteer* experiment=meto7 src/guardoni.js ;
+source=json/fish-control-1.json DEBUG=*,-puppeteer* experiment=meto8 src/guardoni.js ;
 sleep 10;
-source=json/smol.json DEBUG=*,-puppeteer* experiment=meto7 src/guardoni.js ;
+source=json/smol.json DEBUG=*,-puppeteer* experiment=meto8 src/guardoni.js ;
 sleep 10;
-source=json/training1.json DEBUG=*,-puppeteer* experiment=meto7 src/guardoni.js ;
+source=json/training1.json DEBUG=*,-puppeteer* experiment=meto8 src/guardoni.js ;
 sleep 30;
-source=json/fish-control-2.json DEBUG=*,-puppeteer* experiment=meto7 src/guardoni.js 
+source=json/fish-control-2.json DEBUG=*,-puppeteer* experiment=meto8 src/guardoni.js 
