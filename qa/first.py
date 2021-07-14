296aa6365897c0de130f46a50abb7f640b90b2ee
Claudio Agosti <claudio@tracking.exposed>
2021-07-14 15:12:42 +0200

    added a few other APIs and test

--- qa/first.py
+++ qa/first.py
@@ -3,17 +3,23 @@ from ours import fetchContentFromApi
 
 class TestStringMethods(unittest.TestCase):
 
+# https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertFalse
     def test_getLast(self):
         check = fetchContentFromApi('getLast')
-        self.assertEqual(check.id, 'SOME')
+        self.assertGreater(len(check['content']), 1)
+        firstVid = check['content'][0]
+        self.assertGreater(firstVid['secondsago'], 1)
+        self.assertGreater(len(firstVid['title']), 1)
+        self.assertEqual(len(firstVid['videoId']), 11)
+        import pdb; pdb.set_trace()
 
-    def test_isupper(self):
+
+    def test_getLastHome(self):
         check = fetchContentFromApi('getLastHome')
-        self.assertEqual(check.id, 'SOME')
 
-    def test_split(self):
+    def test_getVideoId(self):
         check = fetchContentFromApi('getVideoId')
-        self.assertEqual(check.id, 'SOME')
+
 
 
 if __name__ == '__main__':
--- qa/ours.py
+++ qa/ours.py
@@ -4,15 +4,21 @@ import json
 def getVideoId():
     return "XqZsoesa55w"
 
+def getPublicKey():
+    return "6t43jZSicgEKo6AEbtNW7c66ZoALAxorrG3SKHsf2JnR"
+
 apiSupported = {
     'getLast': '/api/v1/last/',
     'getLastHome': '/api/v1/home/',
-    'getVideoId': '/api/v1/videoId/' + getVideoId()
+    'getVideoId': '/api/v1/videoId/' + getVideoId(),
+    'getRelatedId': '/api/v1/relatedId/' + getVideoId(),
+    'getPersonalCSV_home': '/api/v2/personal/'+getPublicKey()+'/home/csv',
+    'getPersonalCSV_video': '/api/v2/personal/'+getPublicKey()+'/video/csv',
 }
 
 def fetchContentFromApi(apiname):
 
-    print(apiname)
+    print("%s" % apiname)
     # TODO manage remote/production/testing/local server address
     route = 'https://youtube.tracking.exposed' + apiSupported[apiname]
     response = requests.get(route)
