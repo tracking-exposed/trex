1aa4709496f01489458853cc4635495f1635e333
Claudio <claudio@syrianarchive.org>
2021-09-09 18:04:32 +0200

    nothing is better than a fix you imagine even before test it

--- backend/routes/youchoose.js
+++ backend/routes/youchoose.js
@@ -129,23 +129,25 @@ async function creatorRegister(req) {
   const channelId = req.params.channelId;
   const ytvidsurl = `https://www.youtube.com/channel/${channelId}/videos`;
   const { statusCode, data, headers } = await curly.get(ytvidsurl, {
-    verbose: true,
+    verbose: false,
     timeoutMs: 4000,
     sslVerifyPeer: false,
     followLocation: true
   });
 
-  debug("CURL from youtube %d", statusCode);
+  // debug("CURL from youtube %d", statusCode);
   const largestr = data.split('ytInitialData = ')[1].replace(/}};<\/script>.*/g, '}}');
   const blob = JSON.parse(largestr);
   const videob = _.filter(blob.contents.twoColumnBrowseResultsRenderer.tabs,
     function(tabSlot) {
-      if(tabSlot.tabRenderer &&
+      /* if(tabSlot.tabRenderer &&
         tabSlot.tabRenderer.title)
-          debug("%s", tabSlot.tabRenderer.title);
+          debug("%s", tabSlot.tabRenderer.title); */
       return (tabSlot.tabRenderer &&
         tabSlot.tabRenderer.title &&
-        tabSlot.tabRenderer.title === 'Video');
+        tabSlot.tabRenderer.title === 'Videos'); // warning this depends from the server locale
+        // for example in Germany is 
+        // Übersicht Videos Playlists Community Kanäle Kanalinfo
   });
   if(!videob.length) {
     debug("Not found the expected piece in channel %s", channelId);
