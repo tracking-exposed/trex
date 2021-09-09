7cbfcc746b36143a35640a2bf4be09c379ca468d
ascariandrea <dev.ascariandrea@gmail.com>
2021-09-09 15:35:25 +0200

    removed hardcoded value for api config

--- src/chrome/dashboard/API/HTTPAPI.js
+++ src/chrome/dashboard/API/HTTPAPI.js
@@ -1,9 +1,6 @@
 import * as TE from 'fp-ts/lib/TaskEither';
 import * as E from 'fp-ts/lib/Either';
-
-const config = {
-  API_ROOT: 'http://localhost:9000/api/v3'
-};
+import config from '../../../config';
 
 const getURL = (path) => `${config.API_ROOT}${path}`;
 
