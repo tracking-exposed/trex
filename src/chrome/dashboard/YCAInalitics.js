ef66f5f98db295de35bcd885e532376bb9a79313
ascariandrea <dev.ascariandrea@gmail.com>
2021-09-10 10:40:21 +0200

    fix warning error for incompatible nested elements

--- src/chrome/dashboard/YCAInalitics.js
+++ src/chrome/dashboard/YCAInalitics.js
@@ -8,27 +8,27 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     maxWidth: 960,
-    backgroundColor: theme.palette.background.paper
+    backgroundColor: theme.palette.background.paper,
   },
   chip: {
-    margin: theme.spacing(0.5)
+    margin: theme.spacing(0.5),
   },
   section1: {
-    margin: theme.spacing(3, 2)
+    margin: theme.spacing(3, 2),
   },
   section2: {
-    margin: theme.spacing(2)
+    margin: theme.spacing(2),
   },
   section3: {
-    margin: theme.spacing(3, 1, 1)
-  }
+    margin: theme.spacing(3, 1, 1),
+  },
 }));
 
-export default function YCAInalitics () {
+export default function YCAInalitics() {
   const classes = useStyles();
   return (
     <Grid container alignItems="center">
-      <Typography color="textSecondary" variant="body2">
+      <Grid item md={12}>
         <h2>Which videos are recommended close to yours video?</h2>
         <Chip
           className={classes.chip}
@@ -45,20 +45,24 @@ export default function YCAInalitics () {
           color="secondary"
           label="Display recommended videos"
         />
-
+      </Grid>
+      <Grid item md={12}>
         <h2>Where your videos appears as recommended?</h2>
         <Chip className={classes.chip} color="primary" label="Load list" />
-
+      </Grid>
+      <Grid item md={12}>
         <h2>Which advertising get served over your videos?</h2>
         <Chip className={classes.chip} color="primary" label="Load list" />
+      </Grid>
 
+      <Grid item md={12}>
         <h2>Shadow-banning analysis</h2>
         <Chip
           className={classes.chip}
           color="primary"
           label="Load previous tests"
         />
-      </Typography>
+      </Grid>
     </Grid>
   );
 }
