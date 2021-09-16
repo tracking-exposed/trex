621b34a684b58d3fc3f9e9df665f2619d1552ce1
marcfaddoul <marc.faddoul@berkeley.edu>
2021-09-16 09:21:19 +0200

    support CSV responses from API

--- qa/API_fetcher.py
+++ qa/API_fetcher.py
@@ -26,18 +26,28 @@ apiSupported = {
 }
 
 def fetchContentFromApi(apiname):
-
     print("%s" % apiname)
     # TODO manage remote/production/testing/local server address
     route = 'https://youtube.tracking.exposed' + apiSupported[apiname]
     response = requests.get(route)
     try:
         content = json.loads(response.content)
+    # some API return CSV file instead of json
     except:
-        with open(response.content, encoding='utf-8') as csvFile:
-            reader = csv.DictReader(csvFile)
-            content = []
-            for row in reader:
-                content.append(row)
+        import csv
+        import pandas
+        # bytes need to be written to file
+        file_name = '/tmp/API_test_response'
+        response_file = open(file_name, 'wb')
+        response_file.write(response.content)
+        response_file.close()
+        # then open to decode csv
+        response_file = open(file_name, 'r', encoding='utf-8')
+        # skipinitialspace to skip comas within string https://stackoverflow.com/a/8311951/5089456
+        rows = list(csv.reader(response_file, skipinitialspace=True))
+        # return properly formatted json
+        column_names = rows.pop(0)
+        df = pd.DataFrame(rows, columns=column_names)
+        content = df.to_json()
     return content
     # import pdb; pdb.set_trace()
