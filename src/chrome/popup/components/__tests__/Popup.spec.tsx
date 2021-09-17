f91525e1586fe88cd8ea6d6d013549ea5093996d
ascariandrea <dev.ascariandrea@gmail.com>
2021-09-17 12:51:14 +0200

    chore: excluded __tests__ folders from tsc compilation

--- src/chrome/popup/components/__tests__/Popup.spec.tsx
+++ src/chrome/popup/components/__tests__/Popup.spec.tsx
@@ -1,17 +1,11 @@
-import { compose } from 'avenger';
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 import Popup from '../popup';
 
 describe('Popup', () => {
   test('Should mount the Popup', () => {
-    let component = renderer.create(<Popup />);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-
-    // re-rendering
-    component = renderer.create(<Popup publicKey={"pub-key"} />);
-    tree = component.toJSON();
+    const component = renderer.create(<Popup publicKey={'pub-key'} />);
+    const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
--- src/chrome/popup/index.tsx
+++ src/chrome/popup/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Popup from './components/popup';
