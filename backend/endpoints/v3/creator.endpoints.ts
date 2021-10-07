1d78883954eec214e9a607d67257bd20f2b0546a
ascariandrea <dev.ascariandrea@gmail.com>
2021-10-07 14:24:55 +0200

    feat: defined auth endpoint

---
+++ backend/endpoints/v3/creator.endpoints.ts
@@ -0,0 +1,44 @@
+import { Endpoint } from "ts-endpoint";
+import * as t from "io-ts";
+import { AuthResponse } from "../../models/Auth";
+import { ContentCreator } from "../../models/ContentCreator";
+
+const ChannelType = t.literal("channel");
+
+const SingleContentCreatorResponse = ContentCreator;
+
+const GetCreator = Endpoint({
+  Method: "GET",
+  getPath: ({ channelId }) => `/creator/${channelId}`,
+  Input: {
+    Params: t.type({ channelId: t.string }),
+  },
+  Output: SingleContentCreatorResponse,
+});
+
+const RegisterCreator = Endpoint({
+  Method: "POST",
+  getPath: ({ channelId }) => `/creator/${channelId}/register`,
+  Input: {
+    Params: t.type({ channelId: t.string }),
+    Body: t.type({
+      type: ChannelType,
+    }),
+  },
+  Output: AuthResponse,
+});
+
+const VerifyCreator = Endpoint({
+  Method: "POST",
+  getPath: ({ channelId }) => `/creator/${channelId}/verify`,
+  Input: {
+    Params: t.type({ channelId: t.string }),
+  },
+  Output: AuthResponse,
+});
+
+export const endpoints = {
+  GetCreator,
+  RegisterCreator,
+  VerifyCreator,
+};
---
+++ backend/endpoints/v3/index.ts
@@ -0,0 +1,7 @@
+import { endpoints as Creator } from './creator.endpoints';
+
+export const Endpoints = {
+  creator: Creator,
+};
+
+export type Endpoints = typeof Endpoints;
