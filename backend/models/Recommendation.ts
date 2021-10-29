c3aebca2066fdb5a281f19781c525c4498a2935b
ascariandrea <dev.ascariandrea@gmail.com>
2021-10-29 16:48:38 +0200

    fix: content creator stats models

--- backend/models/Recommendation.ts
+++ backend/models/Recommendation.ts
@@ -1,29 +1,29 @@
-import * as t from "io-ts";
-import { NumberFromString } from "io-ts-types/lib/NumberFromString";
+import * as t from 'io-ts';
+import { NumberFromString } from 'io-ts-types/lib/NumberFromString';
 
 export const GetRecommendationsQuery = t.type(
   // to make it "optional" change the codec to `t.union([NumberFromString, t.undefined])`
   { limit: NumberFromString },
-  "GetRecommendationQuery"
+  'GetRecommendationQuery'
 );
 export type GetRecommendationsQuery = t.TypeOf<typeof GetRecommendationsQuery>;
 
 export const GetRecommendationsParams = t.type(
   { ids: t.string },
-  "GetRecommendationParams"
+  'GetRecommendationParams'
 );
 export type GetRecommendationParams = t.TypeOf<typeof GetRecommendationsParams>;
 
 export const Recommendation = t.strict(
   {
-    videoId: t.string,
+    videoId: t.union([t.string, t.undefined]),
     urlId: t.string,
     title: t.string,
     description: t.string,
-    image: t.union([t.undefined, t.string]),
+    image: t.union([t.string, t.undefined]),
   },
 
-  "Recommendation"
+  'Recommendation'
 );
 
 export type Recommendation = t.TypeOf<typeof Recommendation>;
