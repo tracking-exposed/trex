46cef696cb32e5a3d125612ba92628c282476620
ascariandrea <dev.ascariandrea@gmail.com>
2021-10-29 17:53:29 +0200

    feat: added authorization to update creator video

--- backend/endpoints/v3/creator.endpoints.ts
+++ backend/endpoints/v3/creator.endpoints.ts
@@ -1,15 +1,15 @@
-import { Endpoint } from "ts-endpoint";
-import * as t from "io-ts";
-import { AuthorizationHeader, AuthResponse } from "../../models/Auth";
-import { ContentCreator } from "../../models/ContentCreator";
-import { CreatorStats } from "../../models/CreatorStats";
-import { Recommendation } from "../../models/Recommendation";
-import { Video } from "../../models/Video";
+import { Endpoint } from 'ts-endpoint';
+import * as t from 'io-ts';
+import { AuthorizationHeader } from '../../models/Auth';
+import { ContentCreator } from '../../models/ContentCreator';
+import { CreatorStats } from '../../models/CreatorStats';
+import { Recommendation } from '../../models/Recommendation';
+import { Video } from '../../models/Video';
 
-const ChannelType = t.literal("channel");
+const ChannelType = t.literal('channel');
 
 const GetCreator = Endpoint({
-  Method: "GET",
+  Method: 'GET',
   getPath: () => `/v3/creator/me`,
   Input: {
     Headers: AuthorizationHeader,
@@ -18,7 +18,7 @@ const GetCreator = Endpoint({
 });
 
 const RegisterCreator = Endpoint({
-  Method: "POST",
+  Method: 'POST',
   getPath: ({ channelId }) => `/v3/creator/${channelId}/register`,
   Input: {
     Params: t.type({ channelId: t.string }),
@@ -30,7 +30,7 @@ const RegisterCreator = Endpoint({
 });
 
 const VerifyCreator = Endpoint({
-  Method: "POST",
+  Method: 'POST',
   getPath: ({ channelId }) => `/v3/creator/${channelId}/verify`,
   Input: {
     Params: t.type({ channelId: t.string }),
@@ -39,7 +39,7 @@ const VerifyCreator = Endpoint({
 });
 
 const CreatorVideos = Endpoint({
-  Method: "GET",
+  Method: 'GET',
   getPath: () => `/v3/creator/videos`,
   Input: {
     Headers: AuthorizationHeader,
@@ -48,7 +48,7 @@ const CreatorVideos = Endpoint({
 });
 
 const PullCreatorVideos = Endpoint({
-  Method: "POST",
+  Method: 'POST',
   getPath: () => `/v3/creator/videos/repull`,
   Input: {
     Headers: AuthorizationHeader,
@@ -57,17 +57,16 @@ const PullCreatorVideos = Endpoint({
 });
 
 const CreatorRecommendations = Endpoint({
-  Method: "GET",
-  getPath: ({ channelId }) => `/v3/creator/recommendations/${channelId}`,
+  Method: 'GET',
+  getPath: () => `/v3/creator/recommendations`,
   Input: {
-    Params: t.type({ channelId: t.string }),
     Headers: AuthorizationHeader,
   },
   Output: t.array(Recommendation),
 });
 
 const CreatorRelatedChannels = Endpoint({
-  Method: "GET",
+  Method: 'GET',
   getPath: ({ channelId, amount, skip }) =>
     `/v3/creator/${channelId}/related/${amount}-${skip}`,
   Input: {
@@ -78,12 +77,11 @@ const CreatorRelatedChannels = Endpoint({
 });
 
 const UpdateVideo = Endpoint({
-  Method: "POST",
+  Method: 'POST',
   getPath: () => `/v3/creator/updateVideo`,
   Input: {
     Headers: AuthorizationHeader,
     Body: t.type({
-      creatorId: t.string,
       videoId: t.string,
       recommendations: t.array(t.string),
     }),
@@ -92,7 +90,7 @@ const UpdateVideo = Endpoint({
 });
 
 const CreateRecommendation = Endpoint({
-  Method: "POST",
+  Method: 'POST',
   getPath: () => `/v3/creator/ogp`,
   Input: {
     Headers: AuthorizationHeader,
@@ -102,8 +100,8 @@ const CreateRecommendation = Endpoint({
 });
 
 const GetCreatorStats = Endpoint({
-  Method: "GET",
-  getPath: ({channelId}) => `/v3/creator/${channelId}/stats`,
+  Method: 'GET',
+  getPath: ({ channelId }) => `/v3/creator/${channelId}/stats`,
   Input: {
     Params: t.type({ channelId: t.string }),
   },
--- backend/routes/youchoose.js
+++ backend/routes/youchoose.js
@@ -1,35 +1,35 @@
-const _ = require("lodash");
-const moment = require("moment");
-const debug = require("debug")("routes:youchoose");
-const fetchOpengraph = require("fetch-opengraph");
+const _ = require('lodash');
+const moment = require('moment');
+const debug = require('debug')('routes:youchoose');
+const fetchOpengraph = require('fetch-opengraph');
 
-const ycai = require("../lib/ycai");
-const curly = require("../lib/curly");
-const endpoints = require("../lib/endpoint");
-const { v3 } = require("../endpoints");
+const ycai = require('../lib/ycai');
+const curly = require('../lib/curly');
+const endpoints = require('../lib/endpoint');
+const { v3 } = require('../endpoints');
 const structured = require('../lib/structured');
 
 const PUBLIC_AMOUNT_ELEMS = 100;
 
 async function byVideoId(req) {
   /* this function can be invoked in two ways: POST or GET */
-  const source1 = req.params ? _.get(req.params, "videoId") : null;
-  const source2 = req.body ? _.get(req.body, "videoId") : null;
+  const source1 = req.params ? _.get(req.params, 'videoId') : null;
+  const source2 = req.body ? _.get(req.body, 'videoId') : null;
   const videoId = source1 || source2;
-  debug("videoId %s kind %s", videoId, source1 ? "GET/params" : "POST/body");
+  debug('videoId %s kind %s', videoId, source1 ? 'GET/params' : 'POST/body');
   if (!videoId) {
-    debug("Missing mandatory parameter: videoId (%s)", JSON.stringify(req));
-    return { json: { error: true, message: "missing videoId" } };
+    debug('Missing mandatory parameter: videoId (%s)', JSON.stringify(req));
+    return { json: { error: true, message: 'missing videoId' } };
   }
-  debug("Looking recommendations for videoId %s", videoId);
-  const avail = await ycai.fetchRecommendations(videoId, "producer");
+  debug('Looking recommendations for videoId %s', videoId);
+  const avail = await ycai.fetchRecommendations(videoId, 'producer');
   return { json: avail };
 }
 
 async function byProfile(req) {
   const avail = await ycai.fetchRecommendationsByProfile();
   debug(
-    "byProfile (%s) returning without filter %d recommendations",
+    'byProfile (%s) returning without filter %d recommendations',
     req.params.publicKey,
     avail.length
   );
@@ -38,10 +38,10 @@ async function byProfile(req) {
 
 async function ogpProxy(req) {
   const url = req.body.url;
-  debug("ogpProxy: %s", url);
+  debug('ogpProxy: %s', url);
   const exists = await ycai.getRecommendationByURL(url);
   if (exists) {
-    debug("Requested OGP to an already acquired URL %s", url);
+    debug('Requested OGP to an already acquired URL %s', url);
     return {
       json: exists,
     };
@@ -49,7 +49,7 @@ async function ogpProxy(req) {
   const result = await fetchOpengraph.fetch(url);
   const review = await ycai.saveRecommendationOGP(result);
   if (!review.title) {
-    debug("We got an error in OGP (%s) %j", url, review);
+    debug('We got an error in OGP (%s) %j', url, review);
     return {
       json: {
         error: true,
@@ -57,39 +57,45 @@ async function ogpProxy(req) {
       },
     };
   }
-  debug("Fetched correctly %s", url);
+  debug('Fetched correctly %s', url);
   return { json: review };
 }
 
 async function videoByCreator(req) {
   // this function should validate req.params.authMaterial
-  const decodedReq = endpoints.decodeRequest(v3.Endpoints.Creator.CreatorVideos, req);
+  const decodedReq = endpoints.decodeRequest(
+    v3.Endpoints.Creator.CreatorVideos,
+    req
+  );
   if (decodedReq.type === 'error') {
     return {
       json: {
         error: true,
-        details: decodedReq.result
-      }
-    }
+        details: decodedReq.result,
+      },
+    };
   }
   const token = decodedReq.result.headers['x-authorization'];
   const creator = await ycai.getCreatorByToken(token);
   // at the moment the publicKey is the channelId
 
-  debug("Querying DB.ytvids for profile [%s]", creator._id);
+  debug('Querying DB.ytvids for profile [%s]', creator._id);
   const MAXVIDOEL = 100;
-  const videos = await ycai.getVideoFromYTprofiles({ id: creator.channelId }, MAXVIDOEL);
+  const videos = await ycai.getVideoFromYTprofiles(
+    { id: creator.channelId },
+    MAXVIDOEL
+  );
 
   // format: recommendation might be empty or unset
   // creatorId, when, videoId, title, recommendations: []
   const ready = _.map(videos, function (v) {
-    _.unset(v, "_id");
+    _.unset(v, '_id');
     if (!v.recommendations) v.recommendations = [];
     return v;
   });
 
   debug(
-    "Requested Video List by content creator (%s) returning %d",
+    'Requested Video List by content creator (%s) returning %d',
     creator.id,
     ready.length
   );
@@ -98,20 +104,24 @@ async function videoByCreator(req) {
 }
 
 async function repullByCreator(req) {
-  const decodedReq = endpoints.decodeRequest(v3.Endpoints.Creator.PullCreatorVideos, req);
+  const decodedReq = endpoints.decodeRequest(
+    v3.Endpoints.Creator.PullCreatorVideos,
+    req
+  );
   if (decodedReq.type === 'error') {
-    return { json: {
-      error: true,
-      details: decodedReq.result
-      }
-    }
+    return {
+      json: {
+        error: true,
+        details: decodedReq.result,
+      },
+    };
   }
   const token = decodedReq.result.headers['x-authorization'];
-  debug("repullByCreator token %s", token)
+  debug('repullByCreator token %s', token);
   const creator = await ycai.getCreatorByToken(token);
-  debug("repullByCreator %j", creator)
+  debug('repullByCreator %j', creator);
   const titlesandId = await curly.recentVideoFetch(creator.channelId);
-  debug("repull of %j", titlesandId);
+  debug('repull of %j', titlesandId);
   await ycai.registerVideos(titlesandId, creator.channelId);
   return { json: titlesandId };
 }
@@ -119,34 +129,53 @@ async function repullByCreator(req) {
 async function getRecommendationById(req) {
   // this is a public function, anyone can query a recommandation detail
   // this function support a single Id or a list of IDs
-  const paramsResult = endpoints.decodeRequest(v3.Endpoints.Public.GetRecommendations, req);
-  debug("params result %O", paramsResult);
+  const paramsResult = endpoints.decodeRequest(
+    v3.Endpoints.Public.GetRecommendations,
+    req
+  );
+  debug('params result %O', paramsResult);
   if (paramsResult.type === 'error') {
     return {
       json: paramsResult,
     };
   }
-  const ids = paramsResult.result.params.ids.split(",");
+  const ids = paramsResult.result.params.ids.split(',');
 
-  const limit = paramsResult.result.query.limit
+  const limit = paramsResult.result.query.limit;
   const recomms = await ycai.recommendationById(ids, limit);
-  debug("getRecommendationById (%d ids) found %d", ids.length, recomms.length);
+  debug('getRecommendationById (%d ids) found %d', ids.length, recomms.length);
   return { json: recomms };
 }
 
 async function updateVideoRec(req) {
   const update = req.body;
-
-  if (!update.creatorId)
+  const decodedReq = endpoints.decodeRequest(
+    v3.Endpoints.Creator.UpdateVideo,
+    req
+  );
+  if (decodedReq.type === 'error') {
     return {
       json: {
         error: true,
-        message: "missing creatorId — should be replaced with proper auth",
+        details: decodedReq.result
       },
     };
+  }
 
+  const creator = await ycai.getCreatorByToken(
+    decodedReq.result.headers['x-authorization']
+  );
+
+  if (!creator) {
+    return {
+      json: {
+        error: true,
+        message: "Creator doesn't exists",
+      },
+    };
+  }
   if (!update.videoId)
-    return { json: { error: true, message: "missing videoId" } };
+    return { json: { error: true, message: 'missing videoId' } };
 
   if (!update.recommendations || !update.recommendations.length)
     update.recommendations = [];
@@ -157,11 +186,11 @@ async function updateVideoRec(req) {
     })
   )
     return {
-      json: { error: true, message: "validation fail in recommendation list" },
+      json: { error: true, message: 'validation fail in recommendation list' },
     };
 
   debug(
-    "Updating videoId %s with %d recommendations",
+    'Updating videoId %s with %d recommendations',
     update.videoId,
     update.recommendations.length
   );
@@ -175,25 +204,25 @@ async function updateVideoRec(req) {
 }
 
 async function creatorRegister(req) {
-  const channelId = _.get(req.params, "channelId");
+  const channelId = _.get(req.params, 'channelId');
   if (!channelId || channelId.length < 10)
     return {
       json: {
         error: true,
-        message: "channelId missing?!",
+        message: 'channelId missing?!',
       },
     };
 
-  const type = _.get(req.body, "type");
-  if (type !== "channel")
+  const type = _.get(req.body, 'type');
+  if (type !== 'channel')
     return {
       json: {
         error: true,
-        message: "Not supported type?",
+        message: 'Not supported type?',
       },
     };
 
-  const expireAt = moment().add(1, "week").toISOString();
+  const expireAt = moment().add(1, 'week').toISOString();
   const verificationToken = await ycai.generateToken(
     { channelId, type },
     expireAt
@@ -216,7 +245,7 @@ async function creatorVerify(req) {
   const channelId = req.params.channelId;
 
   const tokeno = await ycai.getToken({
-    type: "channel",
+    type: 'channel',
     channelId,
   });
 
@@ -233,103 +262,127 @@ async function creatorVerify(req) {
   debug("Code retrieved %s", pageData.code);
 
   if (tokeno.verificationToken != pageData.code) {
-    debug("Validation fail: %s != %s", tokeno.verificationToken, pageData.code);
+    debug('Validation fail: %s != %s', tokeno.verificationToken, pageData.code);
     return {
       json: {
         error: true,
-        message: "code not found!",
+        message: 'code not found!',
       },
     };
   }
 
-  debug("Validated token, invoking creation with token %o page info %o", tokeno, pageData);
+  debug(
+    'Validated token, invoking creation with token %o page info %o',
+    tokeno,
+    pageData
+  );
   try {
     // two action happens in this function:
     // 1) remove the token
     // 2) create a 'creator' entry with the new auth material
     const creator = await ycai.confirmCreator(tokeno, pageData);
     return {
-      json: creator
+      json: creator,
     };
-  } catch(error) {
+  } catch (error) {
     return {
       json: {
         error: true,
         message: error.message,
-      }
-    }
+      },
+    };
   }
 }
 
 async function creatorGet(req) {
-  // this is the /v3/creator/me query, it looks into 
+  // this is the /v3/creator/me query, it looks into
   // 'creators' mongodb collection.
 
-  const decodedReq = endpoints.decodeRequest(v3.Endpoints.Creator.GetCreator, req);
+  const decodedReq = endpoints.decodeRequest(
+    v3.Endpoints.Creator.GetCreator,
+    req
+  );
   if (decodedReq.type === 'error') {
     return {
       json: {
         error: true,
-        details: decodedReq.result
-      }
-    }
+        details: decodedReq.result,
+      },
+    };
   }
-  const verificationToken = decodedReq.result.headers["x-authorization"];
+  const verificationToken = decodedReq.result.headers['x-authorization'];
   // const channelId = req.headers.channelId;
   // if(!channelId && !verificationToken)
   //   return { json: { error: true, message: "missing channelId or verificationToken in the header"}};
 
-  debug("getCreator by token %s", verificationToken);
+  debug('getCreator by token %s', verificationToken);
   const infoavail = await ycai.getCreatorByToken(verificationToken);
-  const validatedc = endpoints.decodeResponse(v3.Endpoints.Creator.GetCreator, {...infoavail, registeredOn: infoavail.registeredOn.toISOString() });
+  const validatedc = endpoints.decodeResponse(v3.Endpoints.Creator.GetCreator, {
+    ...infoavail,
+    registeredOn: infoavail.registeredOn.toISOString(),
+  });
 
-  if(validatedc.type === 'error') {
-    debug("Invalid generated output for creatorGet %O", validatedc);
-    return { json: {
-        details: validatedc.result
-      }
-    }
+  if (validatedc.type === 'error') {
+    debug('Invalid generated output for creatorGet %O', validatedc);
+    return {
+      json: {
+        details: validatedc.result,
+      },
+    };
   }
   return { json: validatedc.result };
 }
 
 async function creatorDelete(req) {
-  // this function is invoked when a content creator wants to 
+  // this function is invoked when a content creator wants to
   // delete every data on their belong
-  console.log("TODO: Not implemented nor yet specify")
+  console.log('TODO: Not implemented nor yet specify');
   return true;
 }
 
 async function getCreatorStats(req) {
   const amount = PUBLIC_AMOUNT_ELEMS;
   const skip = 0;
-  debug("TODO glue stats by auth %o", req.params);
+  debug('TODO glue stats by auth %o', req.params);
 
-  const decodedReq = endpoints.decodeRequest(v3.Endpoints.Creator.GetCreatorStats, req);
+  const decodedReq = endpoints.decodeRequest(
+    v3.Endpoints.Creator.GetCreatorStats,
+    req
+  );
   const channelId = decodedReq.result.params.channelId;
   const creator = await structured.getChannel(channelId);
-  let authorStruct = await structured.getMetadata({ authorName: creator.username }, {amount, skip});
+  let authorStruct = await structured.getMetadata(
+    { authorName: creator.username },
+    { amount, skip }
+  );
   authorStruct = _.merge(authorStruct, {
     authorSource: creator.channelId,
     authorName: creator.username,
-  })
+  });
   const { units, ready } = structured.buildRecommFlat(authorStruct);
 
-  debug("Returning byAuthor (%o) %d video considered, %d recommendations",
-    creator, _.size(authorStruct.content), _.size(ready) );
+  debug(
+    'Returning byAuthor (%o) %d video considered, %d recommendations',
+    creator,
+    _.size(authorStruct.content),
+    _.size(ready)
+  );
 
-  const retval = endpoints.decodeResponse(v3.Endpoints.Creator.GetCreatorStats, {
-    authorName: authorStruct.authorName,
-    authorSource: authorStruct.authorSource,
-    paging: authorStruct.paging,
-    overflow: authorStruct.overflow,
-    ...units,
-    content: ready,
-  });
+  const retval = endpoints.decodeResponse(
+    v3.Endpoints.Creator.GetCreatorStats,
+    {
+      authorName: authorStruct.authorName,
+      authorSource: authorStruct.authorSource,
+      paging: authorStruct.paging,
+      overflow: authorStruct.overflow,
+      ...units,
+      content: ready,
+    }
+  );
 
-  if(retval.type === 'error') {
-    debug("Invalid generated byAuthor stats! %O", retval);
-    return { json: retval }
+  if (retval.type === 'error') {
+    debug('Invalid generated byAuthor stats! %O', retval);
+    return { json: retval };
   }
   return { json: retval.result };
 }
