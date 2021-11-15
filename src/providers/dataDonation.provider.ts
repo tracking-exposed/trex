8d33b36525186ceb1529bce7bb1453a33dbd6472
Claudio Agosti <claudio@tracking.exposed>
2021-11-15 15:09:14 +0100

    feat: extended selectors closes #110 (#111)

--- src/providers/dataDonation.provider.ts
+++ src/providers/dataDonation.provider.ts
@@ -59,58 +59,44 @@ const observers: MutationObserver[] = [];
 const watchedPaths = {
   banner: {
     selector: '.video-ads.ytp-ad-module',
-    parents: 4,
-    color: 'blue',
-    screen: true,
-  },
+    parents: 4, color: 'blue' },
   ad: {
     selector: '.ytp-ad-player-overlay',
-    parents: 4,
-    color: 'darkblue',
-    screen: true,
-  },
+    parents: 4, color: 'darkblue' },
   overlay: {
     selector: '.ytp-ad-player-overlay-instream-info',
-    parents: 4,
-    color: 'lightblue',
-    screen: true,
-  },
+    parents: 4, color: 'lightblue' },
   toprightad: {
     selector: 'ytd-promoted-sparkles-web-renderer',
-    parents: 3,
-    color: 'aliceblue',
-  },
+    parents: 3, color: 'aliceblue' },
   toprightpict: {
     selector: '.ytd-action-companion-ad-renderer',
-    parents: 2,
-    color: 'azure',
-  },
+    parents: 2, color: 'azure' },
   toprightcta: {
     selector: '.sparkles-light-cta',
-    parents: 1,
-    color: 'violetblue',
-  },
+    parents: 1, color: 'violetblue' },
   toprightattr: {
     selector: '[data-google-av-cxn]',
-    color: 'deeppink',
-  },
+    color: 'deeppink' },
   adbadge: {
     selector: '#ad-badge',
-    parents: 4,
-    color: 'deepskyblue',
-  },
-  // video-ad-overlay-slot
-  channel: {
-    selector: '[href^="/channel/"]',
-    color: 'yellow',
-    parents: 1,
-  },
+    parents: 4, color: 'deepskyblue' },
+  frontad: {
+    selector: 'ytd-banner-promo-renderer' },
+  channel1: {
+    selector: '[href^="/channel"]',
+    color: 'yellow', parents: 1 },
+  channel2: {
+    selector: '[href^="/c"]',
+    color: 'yellow', parents: 1 },
+  channel3: {
+    selector: '[href^="/user"]',
+    color: 'yellow', parents: 1 },
   searchcard: { selector: '.ytd-search-refinement-card-renderer' },
   channellink: { selector: '.channel-link' },
   searchAds: {
     selector: '.ytd-promoted-sparkles-text-search-renderer',
-    parents: 2,
-  },
+    parents: 2 },
 };
 
 const state: CollectedState = {
