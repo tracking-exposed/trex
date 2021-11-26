440313cc49222cf3efbbfe5abda9755f2e0fe815
ascariandrea <dev.ascariandrea@gmail.com>
2021-11-26 15:17:38 +0100

    chore: defined mongo test instance in docker-compose

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,5 +1,7 @@
 const nconf = require("nconf");
 
-nconf.argv().env().file({ file: "config/settings.json" });
+nconf.argv().env().file({ file: "config/settings.json" }).overrides({
+  mongoPort: 27019,
+});
 
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
