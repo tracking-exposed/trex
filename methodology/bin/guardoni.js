ff257c2cca81c309081fec22b53eb74bfd8fdfc2
Claudio <claudio@syrianarchive.org>
2021-12-07 15:38:24 +0100

    progress in #200

--- methodology/bin/guardoni.js
+++ methodology/bin/guardoni.js
@@ -13,7 +13,6 @@ const execSync = require('child_process').execSync;
 const parse = require('csv-parse/lib/sync');
 
 const domainSpecific = require('../src/domainSpecific');
-const { formatWithOptions } = require('util');
 
 const COMMANDJSONEXAMPLE = "https://youtube.tracking.exposed/json/automation-example.json";
 const EXTENSION_WITH_OPT_IN_ALREADY_CHECKED='https://github.com/tracking-exposed/yttrex/releases/download/v1.8.992/extension-1.8.992.zip';
