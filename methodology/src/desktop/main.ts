8cc5eea46c9f035310b71bd59173d78422fe2b83
ascariandrea <dev.ascariandrea@gmail.com>
2021-12-30 17:20:34 +0100

    fix: include extension in distro and use electron-log

--- methodology/src/desktop/main.ts
+++ methodology/src/desktop/main.ts
@@ -3,6 +3,11 @@ import * as path from "path";
 import puppeteer from "puppeteer-core";
 import pie from "puppeteer-in-electron";
 import * as guardoni from "../guardoni";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import { sequenceS } from "fp-ts/lib/Apply";
+import log from "electron-log";
 
 let mainWindow: BrowserWindow | null = null;
 
@@ -10,96 +15,124 @@ const mainWindowHTML = `file://${path.join(
   __dirname,
   "renderer/guardoni.html"
 )}`;
-const creatMainWindow = async (): Promise<BrowserWindow> => {
-  mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    fullscreenable: false,
-    resizable: false,
-    autoHideMenuBar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      devTools: true,
-    },
-  });
-  await mainWindow.loadURL(mainWindowHTML);
-  mainWindow.on("closed", function () {
-    mainWindow = null;
-  });
-  return mainWindow;
+
+const creatMainWindow = (): TE.TaskEither<Error, BrowserWindow> => {
+  return TE.tryCatch(async () => {
+    mainWindow = new BrowserWindow({
+      width: 1000,
+      height: 800,
+      fullscreenable: false,
+      resizable: false,
+      autoHideMenuBar: true,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    });
+
+    await mainWindow.loadURL(mainWindowHTML);
+    mainWindow.on("closed", function () {
+      mainWindow = null;
+    });
+    return mainWindow;
+  }, E.toError);
 };
 
-const createGuardoniWindow = async (
+const createGuardoniWindow = (
   app: Electron.App,
   parentWindow: BrowserWindow
-): Promise<{ browser: puppeteer.Browser; window: BrowserWindow }> => {
-  const browser = await pie.connect(app, puppeteer);
+): TE.TaskEither<
+  Error,
+  { browser: puppeteer.Browser; window: BrowserWindow }
+> => {
+  return TE.tryCatch(async () => {
+    const browser = await pie.connect(app, puppeteer);
 
-  const window = new BrowserWindow({
-    autoHideMenuBar: true,
-    show: false,
-    parent: parentWindow,
-  });
-  return { browser, window };
+    const window = new BrowserWindow({
+      autoHideMenuBar: true,
+      show: false,
+      parent: parentWindow,
+    });
+
+    return { browser, window };
+  }, E.toError);
 };
 
 export const run = async (): Promise<void> => {
+  log.info("Guardoni start", process.cwd());
   app.setPath("userData", path.resolve(process.cwd(), "data"));
 
   await pie.initialize(app);
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   app.on("ready", async () => {
-    const mainWindow = await creatMainWindow();
-    const guardoniApp = await createGuardoniWindow(app, mainWindow);
+    return pipe(
+      creatMainWindow(),
+      TE.chain((win) => createGuardoniWindow(app, win)),
+      TE.chain((guardoniApp) => {
+        return pipe(
+          sequenceS(TE.ApplicativeSeq)({
+            loadExtension: TE.tryCatch(
+              () =>
+                session.defaultSession.loadExtension(
+                  path.join(__dirname, "../extension")
+                ),
+              E.toError
+            ),
+            runGuardoni: TE.tryCatch(async () => {
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              ipcMain.on("startGuardoni", async (event, args) => {
+                const profile = args.profileName ?? "default";
+                const evidenceTag = args.evidenceTag ?? "no-tag";
 
-    await session.defaultSession.loadExtension(
-      path.join(process.cwd(), "./extension")
-    );
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    ipcMain.on("startGuardoni", async (event, args) => {
-      const profile = args.profileName ?? "default";
-      const evidenceTag = args.evidenceTag ?? "no-tag";
+                const experiment = "d75f9eaf465d2cd555de65eaf61a770c82d59451";
 
-      const experiment = "d75f9eaf465d2cd555de65eaf61a770c82d59451";
+                // eslint-disable-next-line no-console
+                log.info("Starting guardoni with", { profile, evidenceTag });
 
-      // eslint-disable-next-line no-console
-      console.log("Starting guardoni with", { profile, evidenceTag });
+                const profileData = await guardoni.profileExecount(
+                  profile,
+                  evidenceTag
+                );
 
-      try {
-        const profileData = await guardoni.profileExecount(
-          profile,
-          evidenceTag
+                // eslint-disable-next-line no-console
+                log.info(profileData);
+
+                const directivesURL = guardoni.buildAPIurl(
+                  "directives",
+                  experiment
+                );
+
+                const directives = await guardoni.pullDirectives(directivesURL);
+
+                // eslint-disable-next-line no-console
+                log.info({ directives });
+
+                await guardoniApp.window.show();
+
+                const page = await pie.getPage(
+                  guardoniApp.browser,
+                  guardoniApp.window
+                );
+                await guardoni.guardoniExecution(
+                  experiment,
+                  directives,
+                  page,
+                  profileData
+                );
+
+                await guardoniApp.window.destroy();
+              });
+            }, E.toError),
+          }),
+          TE.mapLeft((e) => {
+            log.error('An error occured ', e);
+            guardoniApp.window.emit("guardoniError", e);
+            return e;
+          })
         );
-
-        // eslint-disable-next-line no-console
-        console.log(profileData);
-
-        const directivesURL = guardoni.buildAPIurl("directives", experiment);
-
-        const directives = await guardoni.pullDirectives(directivesURL);
-
-        // eslint-disable-next-line no-console
-        console.log({ directives });
-
-        await guardoniApp.window.show();
-
-        const page = await pie.getPage(guardoniApp.browser, guardoniApp.window);
-        await guardoni.guardoniExecution(
-          experiment,
-          directives,
-          page,
-          profileData
-        );
-
-        await guardoniApp.window.destroy();
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.error("An error occured", e);
-        event.sender.send("guardoniError", e);
-      }
-    });
+      })
+    )();
 
     // return undefined;
   });
