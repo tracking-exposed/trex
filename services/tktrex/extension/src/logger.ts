928356ad79ed7e32f192c77082aaed657b08486a
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-01-02 12:12:16 +0100

    feat: improved logger

--- services/tktrex/extension/src/logger.ts
+++ services/tktrex/extension/src/logger.ts
@@ -3,16 +3,45 @@
 // I think it is due to the localStorage, which is not
 // available in the content script and upon which debug relies.
 export default {
-  debug: (...args: unknown[]) => {
+  debug: (msg: string, ...args: unknown[]) => {
     // eslint-disable-next-line no-console
-    console.log('tktrex:debug:', ...args);
+    console.log(
+      `%c(tktrex:debug)%c ${msg}`,
+      [
+        'color: #00f',
+        'background: #F58040',
+        'padding: 3px',
+        'font-weight: bold',
+        'border-radius: 3px',
+      ].join(';'),
+      'color: #222',
+      ...args,
+    );
   },
-  info: (...args: unknown[]) => {
-    // eslint-disable-next-line no-console
-    console.log('tktrex:info:', ...args);
+  info: (msg: string, ...args: unknown[]) => {
+    if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line no-console
+      console.log(
+        `%c(tktrex:info) %c${msg}`,
+        'color: #22F',
+        'color: #000',
+        ...args,
+      );
+    }
   },
-  error: (...args: unknown[]) => {
+  error: (msg: string, ...args: unknown[]) => {
     // eslint-disable-next-line no-console
-    console.error('tktrex:error:', ...args);
+    console.log(
+      `%c(tktrex:error)%c ${msg}`,
+      [
+        'color: #FFF',
+        'background: #F44',
+        'padding: 3px',
+        'font-weight: bold',
+        'border-radius: 3px',
+      ].join(';'),
+      'font-weight: bold',
+      ...args,
+    );
   },
 };
