d52e1f7e2d382a075c72dc6c1a0f190202145686
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-03 11:35:18 +0100

    fix: build extension path

--- methodology/webpack.config.ts
+++ methodology/webpack.config.ts
@@ -1,43 +1,50 @@
-import * as t from "io-ts";
-import * as path from "path";
-import { getConfig } from "../shared/src/webpack/config";
-import { CopyWebpackPlugin } from "../shared/src/webpack/plugins";
+import * as t from 'io-ts';
+import * as path from 'path';
+import { getConfig } from '../shared/src/webpack/config';
+import { CopyWebpackPlugin } from '../shared/src/webpack/plugins';
 
 // process.env.VERSION = packageJson.version;
 
 const { buildENV, ...config } = getConfig({
   cwd: __dirname,
-  outputDir: path.resolve(__dirname, "build/desktop"),
+  outputDir: path.resolve(__dirname, 'build/desktop'),
   env: t.strict({}),
   hot: false,
-  entry: { main: path.resolve(__dirname, "./src/desktop/main.ts") },
+  entry: { main: path.resolve(__dirname, './src/desktop/main.ts') },
 });
 
 // renderer config
 const { buildENV: rendererBuildENV, ...rendererConfig } = getConfig({
   cwd: __dirname,
-  outputDir: path.resolve(__dirname, "build/desktop/renderer"),
+  outputDir: path.resolve(__dirname, 'build/desktop/renderer'),
   env: t.strict({}),
   hot: true,
   entry: {
-    renderer: path.resolve(__dirname, "src/desktop/renderer.tsx"),
+    renderer: path.resolve(__dirname, 'src/desktop/renderer.tsx'),
   },
 });
+
+config.plugins.push(
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: 'extension',
+        to: 'extension',
+      },
+    ],
+  })
+);
 
 rendererConfig.plugins.push(
   new CopyWebpackPlugin({
     patterns: [
       {
-        from: "static",
+        from: 'static',
         filter: (file: string) => {
           const { base } = path.parse(file);
-          return ["guardoni.html"].includes(base);
+          return ['guardoni.html'].includes(base);
         },
       },
-      {
-        from: 'extension',
-        to: 'build/extension'
-      }
     ],
   })
 );
@@ -47,12 +54,12 @@ rendererConfig.plugins.push(
 export default [
   {
     ...rendererConfig,
-    devtool: "source-map",
-    target: "electron-renderer",
+    devtool: 'source-map',
+    target: 'electron-renderer',
   },
   {
     ...config,
-    devtool: "source-map",
-    target: "electron-main",
+    devtool: 'source-map',
+    target: 'electron-main',
   },
 ];
