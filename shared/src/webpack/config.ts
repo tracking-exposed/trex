3c4a8a677bdb9728540bda8fafe827bec7c1d21d
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-05 10:25:40 +0100

    fix: webpack optimized build

--- shared/src/webpack/config.ts
+++ shared/src/webpack/config.ts
@@ -16,7 +16,7 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 const webpackLogger = GetLogger('webpack');
 
-// TODO: babel, browserlist, auto-prefixing, ...?
+// TODO: browserlist, auto-prefixing, ...?
 
 const NODE_ENV = t.union(
   [t.literal('development'), t.literal('test'), t.literal('production')],
@@ -58,7 +58,7 @@ const getConfig = <E extends t.Props>(
     process.env.DOTENV_CONFIG_PATH ??
     path.resolve(opts.cwd, mode === 'production' ? '.env.production' : '.env');
 
-  const tsConfigFile = path.resolve(opts.cwd, './tsconfig.json');
+  // const tsConfigFile = path.resolve(opts.cwd, './tsconfig.json');
 
   webpackLogger.debug(`DOTENV_CONFIG_PATH %s`, DOTENV_CONFIG_PATH);
 
@@ -152,6 +152,8 @@ const getConfig = <E extends t.Props>(
   return {
     mode,
 
+    context: opts.cwd,
+
     entry: opts.entry,
 
     output: {
@@ -169,8 +171,7 @@ const getConfig = <E extends t.Props>(
               loader: 'ts-loader',
               options: {
                 context: opts.cwd,
-                // configFile: require.resolve('./tsconfig.json'),
-                // projectReferences: true,
+                projectReferences: true,
                 transpileOnly: true,
                 getCustomTransformers: () => ({
                   before: [
@@ -205,10 +206,11 @@ const getConfig = <E extends t.Props>(
       extensions: ['.ts', '.tsx', '.js'],
       plugins: [
         new TsconfigPathsPlugin({
-          configFile: tsConfigFile,
+          // configFile: tsConfigFile,
           // context: opts.cwd,
         }),
       ],
+      modules: ['node_modules', path.resolve(opts.cwd)],
     },
 
     plugins,
