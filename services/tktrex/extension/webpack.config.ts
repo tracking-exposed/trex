c3c0037e9c813e30464012d1f560bc350309ca14
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-01-10 16:02:48 +0100

    fix: extension build

--- services/tktrex/extension/webpack.config.ts
+++ services/tktrex/extension/webpack.config.ts
@@ -11,7 +11,8 @@ import { getConfig } from '../../../shared/src/webpack/config';
 import { AppEnv } from './src/AppEnv';
 import packageJSON from './package.json';
 
-const NODE_ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const NODE_ENV =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
 const PRODUCTION = NODE_ENV === 'production';
 const DEVELOPMENT = !PRODUCTION;
 
@@ -27,14 +28,18 @@ const PATHS = {
 };
 
 const DEV_SERVER = 'localhost';
-const ENV_DEP_SERVER = DEVELOPMENT ? ('http://' + DEV_SERVER + ':14000') : 'https://tiktok.tracking.exposed';
-const ENV_DEP_WEB = DEVELOPMENT ? ('http://' + DEV_SERVER + ':1313') : 'https://tiktok.tracking.exposed';
+const ENV_DEP_SERVER = DEVELOPMENT
+  ? 'http://' + DEV_SERVER + ':14000'
+  : 'https://tiktok.tracking.exposed';
+const ENV_DEP_WEB = DEVELOPMENT
+  ? 'http://' + DEV_SERVER + ':1313'
+  : 'https://tiktok.tracking.exposed';
 const LAST_VERSION = 2;
 const BUILDISODATE = new Date().toISOString();
 
 process.env.API_ROOT = `${ENV_DEP_SERVER}/api/v${LAST_VERSION}`;
 process.env.WEB_ROOT = ENV_DEP_WEB;
-process.env.VERSION = `${packageJSON.version}${(DEVELOPMENT ? '-dev' : '')}`;
+process.env.VERSION = `${packageJSON.version}${DEVELOPMENT ? '-dev' : ''}`;
 process.env.BUILD = `On the ${moment().format('DD of MMMM at HH:mm')}.`;
 process.env.BUILDISODATE = BUILDISODATE;
 process.env.FLUSH_INTERVAL = DEVELOPMENT ? '4500' : '9000';
@@ -48,26 +53,31 @@ const { buildENV, ...config } = getConfig({
   entry: PATHS.ENTRY,
   env: AppEnv,
   hot: false,
+  target: 'web',
 });
 
 config.plugins.push(
   new CopyWebpackPlugin({
-    patterns: [{
-      from: 'src/popup',
-    }, {
-      from: 'manifest.json',
-      transform: (content: Buffer) => {
-        const manifest = JSON.parse(content.toString());
-
-        if (NODE_ENV === 'development') {
-          manifest.permissions.push('http://localhost:14000/');
-        }
-
-        return JSON.stringify(manifest, null, 2);
+    patterns: [
+      {
+        from: 'src/popup',
       },
-    }, {
-      from: 'icons',
-    }],
+      {
+        from: 'manifest.json',
+        transform: (content: Buffer) => {
+          const manifest = JSON.parse(content.toString());
+
+          if (NODE_ENV === 'development') {
+            manifest.permissions.push('http://localhost:14000/');
+          }
+
+          return JSON.stringify(manifest, null, 2);
+        },
+      },
+      {
+        from: 'icons',
+      },
+    ],
   }),
 );
 
