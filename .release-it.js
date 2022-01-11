2b1d698714cab8ee99c826e0972105164ee80719
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-11 16:41:08 +0100

    chore: install release-it

---
+++ .release-it.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-template-curly-in-string */
+
+const conventionalCommitTypes = [
+  // MINOR
+  { type: 'feat', release: 'minor' },
+  { type: 'feature', release: 'minor' },
+  // PATCH
+  { type: 'fix', release: 'patch' },
+  { type: 'refactor', release: 'patch' },
+  { type: 'chore', release: 'patch' },
+  { type: 'ci', release: 'patch' },
+  { type: 'test', release: 'patch' },
+  { type: 'bugfix', release: 'patch' },
+  { type: 'hotfix', release: 'patch' },
+  { type: 'perf', release: 'patch' },
+  { type: 'improvement', release: 'patch' },
+  { type: 'revert', release: 'patch' },
+  { type: 'style', release: 'patch' },
+  { type: 'docs', release: 'patch' },
+  // NO RELEASE
+  { type: 'ci', release: false },
+  { type: 'build', release: false },
+  { type: 'release', release: false },
+  { scope: 'no-release', release: false },
+];
+
+module.exports = {
+  hooks: {
+    'after:init': [],
+    'after:bump': [
+      'yarn taboule build',
+      'yarn extension dist',
+      'yarn guardoni build',
+      'yarn guardoni dist',
+      'yarn ycai build:ext',
+    ],
+  },
+  git: {
+    requireUpstream: false,
+    commitMessage: 'release: ${nextRelease.version} CHANGELOG [skip ci]',
+    requireCleanWorkingDir: false,
+  },
+  github: {
+    release: false,
+    assets: [
+      'extension/dist/*.zip',
+      'guardoni/dist/*',
+      'YCAI/build/extension/extension.zip',
+    ],
+  },
+  npm: false,
+  plugins: {
+    '@release-it/conventional-changelog': {
+      infile: 'CHANGELOG.md',
+      preset: {
+        name: 'conventionalcommits',
+        types: conventionalCommitTypes,
+      },
+    },
+    'release-it-yarn-workspaces': {
+      publish: false,
+      skipChecks: true,
+    },
+  },
+};
