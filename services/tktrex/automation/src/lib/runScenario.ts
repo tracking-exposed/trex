dc281fcfcfa0c3376b85bb5321be62e73cac0354
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-01-19 11:59:05 +0100

    feat: captcha detector

--- services/tktrex/automation/src/lib/runScenario.ts
+++ services/tktrex/automation/src/lib/runScenario.ts
@@ -2,14 +2,11 @@ import readline from 'readline';
 
 import puppeteer from 'puppeteer';
 
-import {
-  AutomationScenario,
-  AutomationStep,
-} from '@shared/models/Automation';
+import { AutomationScenario, AutomationStep } from '@shared/models/Automation';
 
 import { CommandConfig } from '../models/CommandCreator';
 
-const prompt = (question: string): Promise<string> => {
+export const prompt = (question: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     const rl = readline.createInterface({
       input: process.stdin,
@@ -23,24 +20,43 @@ const prompt = (question: string): Promise<string> => {
   });
 };
 
+export const sleep = async(ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
 interface Context {
   loggedIn: boolean;
 }
 
+/**
+ * Checks if a captcha is present on the page, if so,
+ * wait until it is not present anymore (and ask user to solve it).
+ */
 export const handleCaptcha = async(
   config: CommandConfig,
   page: puppeteer.Page,
 ): Promise<void> => {
   try {
-    await page.waitForSelector(
-      '#captcha-verify-image',
-    );
+    await page.waitForSelector('#captcha-verify-image', {
+      visible: true,
+      timeout: 5000,
+    });
 
-    await prompt(
-      '[CAPTCHA ALERT] Please solve the captcha then press enter.',
-    );
+    config.log.info('captcha detected, please solve it');
 
-    config.log.info('Thanks for solving the captcha, proceeding...');
+    for (;;) {
+      try {
+        await page.waitForSelector('#captcha-verify-image', {
+          visible: true,
+          timeout: 5000,
+        });
+
+        config.log.info('waiting for captcha to disappear...');
+        await sleep(5000);
+      } catch (err) {
+        config.log.info('thanks for solving the captcha');
+        break;
+      }
+    }
   } catch (err) {
     // ignore
   }
@@ -72,30 +88,29 @@ export const runStep = async(
 };
 
 export const dryRunAutomation =
-(config: CommandConfig) =>
-  async(scenario: AutomationScenario): Promise<void> => {
-    config.log.info('dry-running automation scenario...');
+  (config: CommandConfig) =>
+    async(scenario: AutomationScenario): Promise<void> => {
+      config.log.info('dry-running automation scenario...');
 
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-      args: ['--start-maximized'],
-    });
+      const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized'],
+      });
 
-    const page = await browser.newPage();
+      const page = await browser.newPage();
 
-    const scriptReducer = async(
-      ctx: Promise<Context>,
-      step: AutomationStep,
-    ): Promise<Context> =>
-      runStep(config, await ctx, page, step);
+      const scriptReducer = async(
+        ctx: Promise<Context>,
+        step: AutomationStep,
+      ): Promise<Context> => runStep(config, await ctx, page, step);
 
-    await scenario.script.reduce(
-      scriptReducer,
-      Promise.resolve({
-        loggedIn: false,
-      }),
-    );
+      await scenario.script.reduce(
+        scriptReducer,
+        Promise.resolve({
+          loggedIn: false,
+        }),
+      );
 
-    await browser.close();
-  };
+      await browser.close();
+    };
