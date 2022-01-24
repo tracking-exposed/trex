ef27c66a16f7af226b91736b6bc3eda906504deb
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-24 17:22:04 +0100

    chore: guardoni dist with electron-builder and docker

--- guardoni/scripts/electron-builder.sh
+++ guardoni/scripts/electron-builder.sh
@@ -2,14 +2,17 @@
 
 set -ex
 
-docker run --rm -ti \
+CWD=$PWD/../
+
+docker run -d --name electron-builder-node-16 -t \
  --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
  --env ELECTRON_CACHE="/root/.cache/electron" \
  --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
- -v ${PWD}:/project \
+ -v ${CWD}:/project \
  -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
  electronuserland/builder:16-wine
 
-#  electron-builder build -lw
+docker exec -it electron-builder-node-16 bash -c "yarn"
+docker exec -it electron-builder-node-16 bash -c "yarn guardoni dist"
