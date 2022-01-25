a74a5e6e57ea341ec557e1a4615fa4e68e345250
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-25 16:53:02 +0100

    chore: set host user when running docker container

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,4 +4,11 @@ set -e
 
 DATE=$(date +%m.%y)
 
-docker build --build-arg NODE_VERSION=16.10.0 -t electronuserland/builder:16-wine -t "electronuserland/builder:16-$DATE" -t electronuserland/builder:latest docker/electron-builder
+docker build \
+  --build-arg NODE_VERSION=16.10.0 \
+  --build-arg USER_ID=$(id -u) \
+  --build-arg GROUP_ID=$(id -g) \
+  -t electronuserland/builder:16-wine \
+  -t "electronuserland/builder:16-$DATE" \
+  -t electronuserland/builder:latest \
+  docker/electron-builder
