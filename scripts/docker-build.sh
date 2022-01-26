5b68c903aa35551516a23b80cd54a08c205560b6
Andrea Ascari <dev.ascariandrea@gmail.com>
2022-01-26 08:58:01 +0100

    fix: release-it after bump (#319)

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,4 +4,9 @@ set -e
 
 DATE=$(date +%m.%y)
 
-docker build --build-arg NODE_VERSION=16.10.0 -t electronuserland/builder:16-wine -t "electronuserland/builder:16-$DATE" -t electronuserland/builder:latest docker/electron-builder
+docker build \
+  --build-arg NODE_VERSION=16.10.0 \
+  -t electronuserland/builder:16-wine \
+  -t "electronuserland/builder:16-$DATE" \
+  -t electronuserland/builder:latest \
+  docker/electron-builder
