5faf84c230128979749ed29c128c42a2c9e48f26
Claudio <claudio@syrianarchive.org>
2022-01-29 13:12:37 +0100

    chore: debug in progress — improvement of search parsers

--- backend/parsers/searches.js
+++ backend/parsers/searches.js
@@ -4,109 +4,129 @@ const debuge = require('debug')('parser:searches:error');
 const longlabel = require('./longlabel');
 
 function findSection(video) {
-    // go up till a ytd-shelf-renderer
-    // don't go further a ytd-item-section-renderer
-    let sectionName = "Search results";
-    _.reduce(_.times(5), function(memo) {
-        if(memo.parentNode.tagName === _.toUpper("ytd-item-section-renderer"))
-            return memo;
-        memo = memo.parentNode;
-        const h2 = memo.firstElementChild.querySelector('h2');
-        if(h2) {
-            // check any form of consistency 
-            if(h2.querySelector('.ytd-shelf-renderer'))
-                sectionName = h2.textContent;
-        }
+  // go up till a ytd-shelf-renderer
+  // don't go further a ytd-item-section-renderer
+  let sectionName = 'Search results';
+  _.reduce(
+    _.times(5),
+    function (memo) {
+      if (memo.parentNode.tagName === _.toUpper('ytd-item-section-renderer'))
         return memo;
-    }, video);
-    return sectionName;
+      memo = memo.parentNode;
+      const h2 = memo.firstElementChild.querySelector('h2');
+      if (h2) {
+        // check any form of consistency
+        if (h2.querySelector('.ytd-shelf-renderer'))
+          sectionName = h2.textContent;
+      }
+      return memo;
+    },
+    video
+  );
+  return sectionName;
 }
 
 function dissectVideoAndParents(video, i) {
-    // <ytd-video-renderer> element 
-    const position = i + 1;
-    const thuhref = video.querySelector('a#thumbnail');
-    const href = thuhref.getAttribute('href');
-    if(href.length !== 20)
-        debuge("Not a standard direct link? %s in position %d", href, position);
- 
-    const videoId = href.substr(9);
-    const authorInfo = _.reduce(video.querySelectorAll('a'), function(memo, aele) {
-        const linkto = aele.getAttribute('href');
-        if(!linkto || !linkto.length || !aele.textContent.length)
-            return memo;
-        if(linkto.match(/^\/channel/) || linkto.match(/^\/user/)) {
-            memo.authorName = aele.textContent;
-            memo.authorSource = linkto;
-        }
-        return memo;
-    }, {
-        authorName: null,
-        authorSource: null
-    });
+  // <ytd-video-renderer> element
+  const position = i + 1;
+  const thuhref = video.querySelector('a#thumbnail');
+  const href = thuhref.getAttribute('href');
+  if (href.length !== 20)
+    debuge('Not a standard direct link? %s in position %d', href, position);
 
-    /* if(!authorInfo.authorName)
-        debugger; */
+  const videoId = href.substr(9);
+  const authorInfo = _.reduce(
+    video.querySelectorAll('a'),
+    function (memo, aele) {
+      const linkto = aele.getAttribute('href');
+      if (!linkto || !linkto.length || !aele.textContent.length) return memo;
+      if (linkto.match(/^\/channel/) || linkto.match(/^\/user/)) {
+        memo.authorName = aele.textContent;
+        memo.authorSource = linkto;
+      }
+      return memo;
+    },
+    {
+      authorName: null,
+      authorSource: null,
+    }
+  );
 
-    const title = video
-        .querySelector('[title]')
-        .getAttribute('title');
-    const arialabel = video
-        .querySelector('h3')
-        .querySelector('[aria-label]')
-        .getAttribute('aria-label');
+  /* if(!authorInfo.authorName) debugger; */
 
-    const isLive = !!video.querySelector('.badge-style-type-live-now');
+  const title = video.querySelector('[title]').getAttribute('title');
+  const arialabel = video
+    .querySelector('h3')
+    .querySelector('[aria-label]')
+    .getAttribute('aria-label');
+
+  const isLive = !!video.querySelector('.badge-style-type-live-now');
+
+  try {
     const arinfo = longlabel.parser(arialabel, authorInfo.authorName, isLive);
     const sectionName = findSection(video);
 
     // debug("%d %s", position, sectionName);
     return {
-        position,
-        title,
-        ...authorInfo,
-        sectionName,
-        href,
-        videoId,
-        views: arinfo.views,
-        arialabel,
-        isLive,
-        published: arinfo.timeago ? arinfo.timeago.humanize() : null,
-        secondsAgo: arinfo.timeago ? arinfo.timeago.asSeconds() : NaN,
+      position,
+      title,
+      ...authorInfo,
+      sectionName,
+      href,
+      videoId,
+      views: arinfo.views,
+      arialabel,
+      isLive,
+      order: i,
+      published: arinfo.timeago ? arinfo.timeago.humanize() : null,
+      secondsAgo: arinfo.timeago ? arinfo.timeago.asSeconds() : NaN,
     };
+  } catch (error) {
+    debuge('Failed video #%d: %s', i, error.message);
+    return null;
+  }
 }
 
 function unpackCorrection(corelem) {
-    if(corelem.tagName === 'YT-FORMATTED-STRING')
-        return corelem.textContent;
-    return _.compact(_.flatten(_.map(corelem.children, unpackCorrection)));
+  if (corelem.tagName === 'YT-FORMATTED-STRING') return corelem.textContent;
+  return _.compact(_.flatten(_.map(corelem.children, unpackCorrection)));
 }
 
 function process(envelop) {
-    /* this function process a page like: 
+  /* this function process a page like: 
     https://www.youtube.com/results?search_query=fingerprinting
        and the logic here is: look for any video, and then move above 
        until you figure if belong to a 'macrosection' ("For You", "People Also Watched")
        or not. By using ytd-video-renderer the order is attributed */
 
-    const videos = envelop.jsdom.querySelectorAll('ytd-video-renderer');
-    if(!videos.length) {
-        debuge("Search result of %s doesn't seem having any video!",
-            envelop.impression.nature.query);
-        return null;
-    }
-    const results = _.map(videos, dissectVideoAndParents);
-    const correction = envelop.jsdom.querySelector('yt-search-query-correction');
+  const videos = envelop.jsdom.querySelectorAll('ytd-video-renderer');
+  if (!videos.length) {
+    debuge(
+      "Search result of %s doesn't seem having any video!",
+      envelop.impression.nature.query
+    );
+    return null;
+  }
+  const dissected = _.map(videos, dissectVideoAndParents);
+  const results = _.compact(dissected);
+  if (dissected.length !== results.length) {
+    debuge(
+      'From %d potential vidoes only %d were extracted: rejecting',
+      dissected.length,
+      results.length
+    );
+    return null;
+  }
+  const correction = envelop.jsdom.querySelector('yt-search-query-correction');
 
-    const retval = {
-        ...envelop.impression.nature,
-        results
-    };
-    /* this is the only optional field, and might have two or four elements in the list */
-    if(correction)
-        retval.correction = unpackCorrection(correction);
+  const retval = {
+    ...envelop.impression.nature,
+    results,
+  };
+  /* this is the only optional field, and might have two or four elements in the list */
+  if (correction) retval.correction = unpackCorrection(correction);
 
-    return retval;
+  return retval;
 }
 
 /*
@@ -127,5 +147,5 @@ function process(envelop) {
  */
 
 module.exports = {
-    process,
+  process,
 };
