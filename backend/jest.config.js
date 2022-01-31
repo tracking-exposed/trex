353614b88d19f3c9aeb88c2cbcf72a8ef6ea0e53
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-31 12:21:16 +0100

    test: fix backend configuration

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,40 +1,44 @@
-const tsConfig = require("./tsconfig.json");
+const tsConfig = require('./tsconfig.json');
 // jest.config.js
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { jsWithTsESM } = require("ts-jest/presets");
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { jsWithTsESM } = require('ts-jest/presets');
 
 const moduleNameMapper = pathsToModuleNameMapper(
   tsConfig.compilerOptions.paths,
   {
-    prefix: "<rootDir>",
+    prefix: '<rootDir>',
   }
 );
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["./", "../packages/shared"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['./', '../packages/shared'],
   projects: ['./', '../packages/shared'],
   moduleNameMapper,
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       // TS reports strange errors with jest,
       // that it doesn't report when running plain tsc...
       diagnostics: false,
       isolatedModules: true,
-      useESM: true
+      useESM: true,
+      project: './tsconfig.json',
     },
   },
   transform: {
     ...jsWithTsESM.transform,
-    '\\.js$': 'ts-jest'
+    '\\.js$': 'ts-jest',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)/)',
+  ],
   clearMocks: true,
   testTimeout: 10000,
-  setupFilesAfterEnv: ["./jest.setup.js"],
-  coverageProvider:"v8",
-  collectCoverageFrom: ["./bin/**", "./lib/**", "./routes/**", "./parsers/**"],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['./bin/**', './lib/**', './routes/**', './parsers/**'],
   coverageThreshold: {
     global: {
       branches: 30,
@@ -43,5 +47,5 @@ module.exports = {
       statements: 60,
     },
   },
-  coveragePathIgnorePatterns: ["node_modules"],
+  coveragePathIgnorePatterns: ['node_modules'],
 };
