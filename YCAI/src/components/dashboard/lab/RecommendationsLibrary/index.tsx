ab7cf59ca9feca840fe2a1d3ab747b3ebeb3c3d6
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-31 13:02:38 +0100

    feat: create recommendations library page

---
+++ YCAI/src/components/dashboard/lab/RecommendationsLibrary/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Box } from '@material-ui/core';
+
+const RecommendationsLibrary: React.FC = () => {
+  return <Box>Recommendations Library</Box>;
+};
+
+export default RecommendationsLibrary;
