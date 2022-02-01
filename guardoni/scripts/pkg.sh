1539ea6ca1da62b2cddbe40ca50b9b7c0350bde1
Andrea Ascari <dev.ascariandrea@gmail.com>
2022-02-01 10:46:10 +0100

    fix: guardoni cli execution (#337)

--- guardoni/scripts/pkg.sh
+++ guardoni/scripts/pkg.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh
 
+set -ex
 # this script is invoked by npm run pkg, and simply should rename the executabled with the proper version name.
+
+pkg ./package.json
 
 version=`grep version package.json | cut -b 15- | sed -es/\".*//`
 # 1.8.2
