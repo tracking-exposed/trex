5734da05adb3484ca427280004cf8e39d7eb1cf8
Andrea Ascari <dev.ascariandrea@gmail.com>
2022-02-02 12:24:07 +0100

    chore(guardoni): dist-all shell script (#324)

--- scripts/release-it-after-bump.sh
+++ scripts/release-it-after-bump.sh
@@ -5,16 +5,13 @@ set -e -x
 export DEBUG=@trex:*
 export NODE_ENV=production
 
-# build needed docker images
-yarn docker-build
-yarn docker-run
-
 yarn shared build
 yarn taboule build
 yarn extension dist
 yarn extension dist:guardoni
 yarn guardoni build
 yarn guardoni pkg
-# yarn guardoni dist:all
+yarn guardoni dist:all
+
 # yarn tk:ext build
 yarn ycai build:ext
