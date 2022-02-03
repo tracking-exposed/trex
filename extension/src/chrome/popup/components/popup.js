430ee1a2ee704139e35ee9a3dd7bdbe9a232c381
ascariandrea <dev.ascariandrea@gmail.com>
2022-02-03 13:05:20 +0100

    chore(yttrex): removed outdated create-react-class dep

--- extension/src/chrome/popup/components/popup.js
+++ extension/src/chrome/popup/components/popup.js
@@ -5,7 +5,7 @@ import { Card } from '@material-ui/core';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
-import InfoBox from './infoBox';
+import InfoBox from './InfoBox';
 import Settings from './settings';
 import GetCSV from './getCSV';
 
@@ -15,66 +15,74 @@ import config from '../../../config';
 const bo = chrome || browser;
 
 const styles = {
-    width: '400px',
+  width: '400px',
 };
 
-class Popup extends React.Component{
-
-  constructor (props) {
-      super(props);
-      this.state = { status: 'fetching' };
-      try {
-        bo.runtime.sendMessage({ type: 'localLookup' }, (userSettings) => {
-          console.log("here got", userSettings);
-          if(userSettings && userSettings.publicKey)
-            this.setState({ status: 'done', data: userSettings });
-          else
-            this.setState({ status: 'error', data: userSettings });
-        });
-      } catch(e) {
-        console.log("catch error", e.message, runtime.lastError);
-        this.state = { status: 'error', data: ''};
-      }
+class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: 'fetching' };
+    try {
+      bo.runtime.sendMessage({ type: 'localLookup' }, (userSettings) => {
+        console.log('here got', userSettings);
+        if (userSettings && userSettings.publicKey)
+          this.setState({ status: 'done', data: userSettings });
+        else this.setState({ status: 'error', data: userSettings });
+      });
+    } catch (e) {
+      console.log('catch error', e.message, runtime.lastError);
+      this.state = { status: 'error', data: '' };
     }
+  }
 
-  render () {
-      const version = config.VERSION;
-      const timeago = moment.duration(moment() - moment(config.BUILDISODATE)).humanize() + ' ago';
+  render() {
+    const version = config.VERSION;
+    const timeago =
+      moment.duration(moment() - moment(config.BUILDISODATE)).humanize() +
+      ' ago';
 
-      if(!this.state)
-        return (<div>Loading...</div>)
+    if (!this.state) return <div>Loading...</div>;
 
-      console.log('popup props status', this.props, this.state);
+    console.log('popup props status', this.props, this.state);
 
-      if(this.state.status !== 'done') {
-        console.log("Incomplete info before render");
-        return (
-          <div style={styles}>
-            <Card>
-                <Alert severity="error">
-                    <AlertTitle>Error</AlertTitle>
-                    Extension isn't initialized yet — <strong>Access <a href="https://www.youtube.com" target="_blank">yutube.com</a>.</strong>
-                </Alert>
-                <InfoBox />
-            </Card>
-          </div>
-        );
-      }
-
+    if (this.state.status !== 'done') {
+      console.log('Incomplete info before render');
       return (
         <div style={styles}>
           <Card>
-              <FormHelperText>ytTREX main switch</FormHelperText>
-              <Settings {...this.state.data} />
-              <FormHelperText>Access to your data</FormHelperText>
-              <GetCSV publicKey={this.state.data.publicKey } />
-              <FormHelperText>About</FormHelperText>
-              <InfoBox />
+            <Alert severity="error">
+              <AlertTitle>Error</AlertTitle>
+              Extension isn't initialized yet —{' '}
+              <strong>
+                Access{' '}
+                <a href="https://www.youtube.com" target="_blank">
+                  yutube.com
+                </a>
+                .
+              </strong>
+            </Alert>
+            <InfoBox />
           </Card>
-          <small>version {version}, released {timeago}</small>
         </div>
       );
     }
+
+    return (
+      <div style={styles}>
+        <Card>
+          <FormHelperText>ytTREX main switch</FormHelperText>
+          <Settings {...this.state.data} />
+          <FormHelperText>Access to your data</FormHelperText>
+          <GetCSV publicKey={this.state.data.publicKey} />
+          <FormHelperText>About</FormHelperText>
+          <InfoBox />
+        </Card>
+        <small>
+          version {version}, released {timeago}
+        </small>
+      </div>
+    );
+  }
 }
 
 export default Popup;
