4afe779812c787871dc8bf7ba4a6c0d2d3b90e12
spaghettinucleari <spaghettinucleari@gmail.com>
2022-02-03 14:28:47 +0100

    style: changing icons

--- YCAI/src/components/dashboard/lab/RecommendationsLibrary/RecommendationsList.tsx
+++ YCAI/src/components/dashboard/lab/RecommendationsLibrary/RecommendationsList.tsx
@@ -2,7 +2,7 @@ import { List, Typography } from '@material-ui/core';
 import { Recommendation } from '@shared/models/Recommendation';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { RecommendationCard } from '../RecommendationCard';
+import { RecommendationCard } from '../RecommendationCardLibrary';
 
 interface RecommendationListProps {
   recommendations: Recommendation[];
