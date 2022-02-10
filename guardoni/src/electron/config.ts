0dacbb0938e9749a96fa67e78c2177974b879cfa
ascariandrea <dev.ascariandrea@gmail.com>
2022-02-10 16:56:45 +0100

    fix(guardoni): default paths for guardoni electron

--- guardoni/src/electron/config.ts
+++ guardoni/src/electron/config.ts
@@ -1,8 +1,14 @@
-export const DEFAULT_BASE_PATH = process.cwd();
+import os from 'os';
+import * as path from 'path';
+
+export const DEFAULT_BASE_PATH = path.resolve(
+  os.homedir(),
+  '.guardoni/electron'
+);
 export const DEFAULT_SERVER = 'https://youtube.tracking.exposed/api';
-export const DEFAULT_EXTENSION_DIR = [
+export const DEFAULT_EXTENSION_DIR = path.resolve(
   DEFAULT_BASE_PATH,
-  'build/extension',
-].join('/');
+  'extension'
+);
 
 export const DEFAULT_LOAD_FOR = 3000;
