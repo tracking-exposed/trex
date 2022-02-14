37d919d3ffc5b31e82ae4f0d53b1c5e9b77d4a51
Claudio Agosti <claudio@syrianarchive.org>
2022-02-14 10:30:40 +0100

    fix(backend): memory safer function for experiment extension (#393)

--- backend/bin/ogresolver.js
+++ backend/bin/ogresolver.js
@@ -4,25 +4,23 @@ const debug = require('debug')('bin:ogresolver');
 const fetchOpengraph = require('fetch-opengraph');
 const nconf = require('nconf');
 
-nconf.argv().env().file({ file: 'config/settings.json'});
+nconf.argv().env().file({ file: 'config/settings.json' });
 
 async function start() {
-
   for (const pourl of process.argv) {
-    if(_.startsWith(pourl, 'https://')) {
-      debug("Fetching %s", pourl);
+    if (_.startsWith(pourl, 'https://')) {
+      debug('Fetching %s', pourl);
       const result = await fetchOpengraph.fetch(pourl);
-      const fields = ['title', 'description', 'url', 'image']
+      const fields = ['title', 'description', 'url', 'image'];
       const keep = _.pick(result, fields);
       // eslint-disable-next-line no-console
-      console.log(keep.title);
+      console.log(keep);
     }
-  };
+  }
 }
-
 
 try {
   start();
-} catch(error) {
-  debug("Unexpected error: %s", error.message);
+} catch (error) {
+  debug('Unexpected error: %s', error.message);
 }
