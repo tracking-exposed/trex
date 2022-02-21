139fadb4bd9e1677ad6a0a3b58de63a7147c4e28
ascariandrea <dev.ascariandrea@gmail.com>
2022-02-21 13:07:03 +0100

    fix(ycai): empty gem collection message in yt video page

--- YCAI/src/components/injected/VideoRecommendations.tsx
+++ YCAI/src/components/injected/VideoRecommendations.tsx
@@ -1,10 +1,9 @@
-import { Grid, Typography, Link } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
+import { Recommendation } from '@shared/models/Recommendation';
 import React from 'react';
 import { Trans } from 'react-i18next';
 import { LazyFullSizeLoader } from '../common/FullSizeLoader';
 import { InjectedRecommendationCard } from './InjectedRecommendationCard';
-import { Recommendation } from '@shared/models/Recommendation';
-import { config } from '../../config';
 
 interface VideoRecommendationsProps {
   recommendations: Recommendation[];
@@ -14,15 +13,16 @@ interface VideoRecommendationsProps {
 export const VideoRecommendations: React.FC<VideoRecommendationsProps> = ({
   recommendations,
   loading,
-}) =>
-  loading ? (
+}) => {
+  return loading ? (
     <LazyFullSizeLoader />
   ) : recommendations.length === 0 ? (
     <Typography style={{ fontSize: '1.4rem' }}>
-      <Trans i18nKey="ytVideoPage:noCCRecommendations">
-        This creator has not uploaded customized recommendations yet. If you own
-        this channel, you can do it
-        <Link href={config.PUBLIC_URL}>here</Link>.
+      <Trans
+        i18nKey="ytVideoPage:noCCRecommendations"
+        value={{ subject: 'video' }}
+      >
+        No recommendations on this {{ subject: 'video' }} yet.
       </Trans>
     </Typography>
   ) : (
@@ -34,3 +34,4 @@ export const VideoRecommendations: React.FC<VideoRecommendationsProps> = ({
       ))}
     </Grid>
   );
+};
