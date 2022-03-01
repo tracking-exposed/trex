8a17e4cd640c5dbb505f3b3e459b8061796ed93e
Andrea Ascari <dev.ascariandrea@gmail.com>
2022-03-01 10:18:40 +0100

    refactor(ycai): moved project inside services (#425)

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ const jestBaseConfig = require('./jest.config.base');
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   ...jestBaseConfig,
-  projects: ['<rootDir>/backend', '<rootDir>/guardoni', '<rootDir>/YCAI'],
+  projects: [
+    '<rootDir>/backend',
+    '<rootDir>/guardoni',
+    '<rootDir>/services/ycai/studio',
+  ],
 };
