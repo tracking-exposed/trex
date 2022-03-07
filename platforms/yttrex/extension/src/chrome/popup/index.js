435b25c55b6232b127d5de3d2f97e23309e1c679
ascariandrea <dev.ascariandrea@gmail.com>
2022-03-07 11:40:47 +0100

    perf(yttrex): webpack configuration

--- platforms/yttrex/extension/src/chrome/popup/index.js
+++ platforms/yttrex/extension/src/chrome/popup/index.js
@@ -7,29 +7,29 @@ import Popup from './components/popup';
 const bo = chrome || browser;
 
 const Zimplon = {
-    fontFamily: 'Trex-Regular',
-    fontStyle: 'normal',
-    fontDisplay: 'swap',
-    fontWeight: 400,
-    src: `
+  fontFamily: 'Trex-Regular',
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  fontWeight: 400,
+  src: `
         local('Trex-Regular'),
         url('Trex-Regular.ttf') format('ttf')
     `,
-    unicodeRange:
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+  unicodeRange:
+    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
 };
 
 const theme = createMuiTheme({
-    typography: {
-        fontFamily: 'Trex-Regular'
+  typography: {
+    fontFamily: 'Trex-Regular',
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [Zimplon],
+      },
     },
-    overrides: {
-        MuiCssBaseline: {
-            '@global': {
-                '@font-face': [Zimplon],
-            },
-        },
-    },
+  },
 });
 
 /*
@@ -38,20 +38,27 @@ if (config.NODE_ENV == 'development') {
     styles['backgroundImage'] = devColors;
 } */
 
-function main () {
-    bo.runtime.sendMessage({
-        type: 'localLookup',
-        payload: {
-            userId: 'local'
-        }
-    }, ucfg => {
-        const publicKey = (ucfg && _.isString(ucfg.publicKey) ) ? ucfg.publicKey : 'missingPublicKey';
-        ReactDOM.render(
-            <ThemeProvider theme={theme}>
-                <Popup publicKey={publicKey} />
-            </ThemeProvider>, document.getElementById('main')
-        );
-    });
+function main() {
+  bo.runtime.sendMessage(
+    {
+      type: 'localLookup',
+      payload: {
+        userId: 'local',
+      },
+    },
+    (ucfg) => {
+      const publicKey =
+        ucfg && _.isString(ucfg.publicKey)
+          ? ucfg.publicKey
+          : 'missingPublicKey';
+      ReactDOM.render(
+        <ThemeProvider theme={theme}>
+          <Popup publicKey={publicKey} />
+        </ThemeProvider>,
+        document.getElementById('main')
+      );
+    }
+  );
 }
 
 main();
