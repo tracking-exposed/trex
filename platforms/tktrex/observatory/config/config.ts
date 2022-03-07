fdfb1e9177bf0882affa0737186a46b591948fba
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-03-07 17:14:20 +0100

    feat: runner in TypeScript

--- platforms/tktrex/observatory/config/config.ts
+++ platforms/tktrex/observatory/config/config.ts
@@ -4,3 +4,5 @@ export const massPath = join(__dirname, '../mass');
 export const parsedPath = join(__dirname, '../parsed');
 export const maxParallelWorkers = 10;
 export const MONGO_URL = 'mongodb://localhost:27017/';
+export const LUM_USER = 'lum-customer-X_XXXXXXXX-zone-residential-country';
+export const LUM_PASSWORD = 'password - change me and LUM_USER';
