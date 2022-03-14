002b888b72cdeae29937e1a4529b44c619517ff3
Claudio <claudio@syrianarchive.org>
2022-03-14 14:27:08 +0100

    feat(tktrex): extended mail subscription and listing

--- platforms/tktrex/backend/routes/emails.js
+++ platforms/tktrex/backend/routes/emails.js
@@ -1,7 +1,10 @@
 const nconf = require('nconf');
 const debug = require('debug')('routes:emails');
+const _ = require('lodash');
 
 const mongo3 = require('../lib/mongo3');
+const utils = require('../lib/utils');
+const security = require('../lib/security');
 
 function validateEmail(email) {
   const re =
@@ -9,7 +12,7 @@ function validateEmail(email) {
   return re.test(email);
 }
 
-async function registerEmail(req) {
+async function registerEmail2(req) {
   const email = req.body.email;
   const reason = req.body.reason;
   const collection = nconf.get('schema').emails;
@@ -18,9 +21,16 @@ async function registerEmail(req) {
     debug('Rejected email address %s as invalid', email);
     return { status: 403 };
   }
+  const reasons = ['subscribe-to-ukraine', 'press--list'];
+  if (reasons.indexOf(reason) === -1) {
+    debug('Invalid reason %s not found in %j', reason, reasons);
+    return { status: 403 };
+  }
+
+  const subid = utils.hash({ email, reason });
 
   const mongoc = await mongo3.clientConnect({ concurrency: 1 });
-  const emailAlreadyExists = await mongo3.count(mongoc, collection, { email });
+  const emailAlreadyExists = await mongo3.count(mongoc, collection, { subid });
 
   if (emailAlreadyExists === 1) {
     debug('Address %s already present', email);
@@ -29,6 +39,7 @@ async function registerEmail(req) {
   }
 
   await mongo3.writeOne(mongoc, collection, {
+    subid,
     email,
     registeredAt: new Date(),
     reason,
@@ -38,6 +49,23 @@ async function registerEmail(req) {
   return { status: 200 };
 }
 
+async function listEmails(req) {
+  if (!security.checkPassword(req)) return { status: 403 };
+
+  const mongoc = await mongo3.clientConnect({ concurrency: 1 });
+  const mails = await mongo3.read(mongoc, nconf.get('schema').emails);
+
+  debug(
+    'Fetched %d emails, reasons: %j',
+    mails.length,
+    _.countBy(mails, 'reason')
+  );
+  await mongoc.close();
+
+  return { json: mails };
+}
+
 module.exports = {
-  registerEmail,
+  registerEmail2,
+  listEmails,
 };
