7db469abd77c4d520b68336d5e378e452bd4e130
Claudio Agosti <claudio@syrianarchive.org>
2022-04-06 13:45:33 +0200

    feat: tiktok global observatory backend (#484)
    * feat(tktrex): implemented working prototype of tiktok global observatory backend
    
    * fix(tktrex): added optional parameter on tto route

---
+++ platforms/tktrex/backend/routes/observatory.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+const debug = require('debug')('routes:observatory');
+
+const nconf = require('nconf');
+const mongo3 = require('../lib/mongo3');
+
+async function getCountryFeed(req) {
+  const amount = req.params.amount || 4;
+  debug('requested %s sample amount %d', req.params.country, amount);
+  const samplesAmount = 9 * amount;
+  const filter = { countryCode: req.params.country };
+  const mongoc = await mongo3.clientConnect({ concurrency: 1 });
+  const metadata = await mongo3.readLimit(
+    mongoc,
+    nconf.get('schema').metadata,
+    filter,
+    { creationTime: -1 },
+    samplesAmount,
+    0
+  );
+  await mongoc.close();
+  debug(
+    'fetched %d videos from %s (samplesAmount %d)',
+    _.size(metadata),
+    filter.countryCode,
+    samplesAmount
+  );
+
+  return {
+    json: metadata,
+  };
+}
+
+module.exports = {
+  getCountryFeed,
+};
