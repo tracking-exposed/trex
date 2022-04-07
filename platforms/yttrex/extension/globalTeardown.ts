b57fe0e4c0d95ae73088cbbcb3797fc86ba387d2
ascariandrea <dev.ascariandrea@gmail.com>
2022-04-07 14:54:25 +0200

    test: global setup without jest-dev-server

--- platforms/yttrex/extension/globalTeardown.ts
+++ platforms/yttrex/extension/globalTeardown.ts
@@ -1,5 +1,3 @@
-import { teardown as teardownServer } from 'jest-dev-server';
-
 export default async function globalSetup(): Promise<void> {
-  await teardownServer();
-};
+  await Promise.resolve();
+}
