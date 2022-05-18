9816042903cce12eb5e6af4132460cd9fe7f19c8
ascariandrea <dev.ascariandrea@gmail.com>
2022-05-18 17:06:30 +0200

    chore: better scripts for build and test

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,22 +12,11 @@ yarn workspaces foreach run clean
 # build shared
 yarn shared build
 
-# build yt backend
-yarn yt:backend build
-
-# bootstrap yttrex backend processes
-yarn pm2 start ./platforms/yttrex/backend/ecosystem.config.js
-yarn pm2 save
-
-# test backend
-# yarn yt:backend test
-
 # build yttrex extension
 yarn yt:ext build
 
 # build tktrex extension
 yarn tk:ext build
-
 
 # build guardoni
 yarn guardoni build
