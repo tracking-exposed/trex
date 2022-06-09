1db3efca19922d7ab1f37ac8eed435b3235e01e9
ascariandrea <dev.ascariandrea@gmail.com>
2022-06-09 16:48:53 +0200

    chore: split guardoni cli webpack config from app one

--- platforms/guardoni/scripts/dist-all.sh
+++ platforms/guardoni/scripts/dist-all.sh
@@ -4,7 +4,7 @@ set -e -x
 
 export NODE_ENV='production'
 
-yarn guardoni build
+yarn guardoni build:app
 yarn dist:linux
 # yarn dist:windows
 # yarn dist:mac
--- scripts/release-it-after-bump.sh
+++ scripts/release-it-after-bump.sh
@@ -8,9 +8,11 @@ yarn shared build
 yarn taboule build
 yarn yt:ext dist
 yarn yt:ext dist:guardoni
-yarn guardoni build
-yarn guardoni pkg
 yarn tk:ext build
+yarn guardoni build:cli
+yarn guardoni pkg
+yarn guardoni build:app
+
 
 export DOTENV_CONFIG_PATH=$YCAI_DOTENV_CONFIG_PATH
 yarn ycai build
