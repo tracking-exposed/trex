4d17048e8e448cb9a78bcf12981a0f6abd88cdd4
Claudio Agosti <claudio@syrianarchive.org>
2022-07-01 18:15:24 +0200

    sequences of patches before deploy new extensions and backend (#572)
    * chore(tktrex): removed not streamlined workspaces
    
    * fix(tktrex): extension records evidenceTag and show the popup
    
    * fix: 'secretKey' typo in webpack config
    
    * fix(tktrex): extension button in popup now compacted to be three
    
    * chore: update yarn.lock with removed deps
    
    * chore: disable tk-observatory flow for now
    
    * chore(ycai): transform ignore pattern for tests
    
    * fix(tktrex): added researchTag in any payload if configured in the popup, + renamed variables
    
    * fix(tktrex): changed method for researchTag communication, saved in metadata
    
    * fix(tktrex): returned UTF-8 tags instead of the URLencoded #555
    
    Co-authored-by: ascariandrea <dev.ascariandrea@gmail.com>

--- platforms/tktrex/backend/lib/CSV.js
+++ platforms/tktrex/backend/lib/CSV.js
@@ -3,49 +3,51 @@ const debug = require('debug')('lib:CSV');
 const moment = require('moment');
 
 function produceCSVv1(entries) {
+  const keys = _.keys(entries[0]);
 
-    const keys = _.keys(entries[0]);
+  const produced = _.reduce(
+    entries,
+    function (memo, entry, cnt) {
+      if (!memo.init) {
+        memo.expect = _.size(keys);
+        memo.csv = _.trim(JSON.stringify(keys), '][') + '\n';
+        memo.init = true;
+      }
 
-    let produced = _.reduce(entries, function(memo, entry, cnt) {
-        if(!memo.init) {
-            memo.expect = _.size(keys);
-            memo.csv = _.trim(JSON.stringify(keys), '][') + "\n";
-            memo.init = true;
+      if (_.size(keys) !== memo.expect) {
+        debug(
+          'Invalid JSON input: expected %d keys, got %d',
+          memo.expect,
+          _.size(keys)
+        );
+        // eslint-disable-next-line no-console
+        console.log(memo.csv);
+        // eslint-disable-next-line no-console
+        console.log(JSON.stringify(entry, undefined, 2));
+        throw new Error('Format error');
+      }
+
+      _.each(keys, function (k, i) {
+        let swap = _.get(entry, k, '');
+        if (k === 'savingTime') memo.csv += moment(swap).toISOString();
+        else if (_.isInteger(swap)) memo.csv += swap;
+        else if (k === 'related') {
+          debug('Ignored related content'); // is this a skeleton?
+        } else {
+          swap = _.replace(swap, /"/g, '〃');
+          swap = _.replace(swap, /'/g, '’');
+          memo.csv += '"' + swap + '"';
         }
-
-        if(_.size(keys) != memo.expect) {
-            debug("Invalid JSON input: expected %d keys, got %d",
-                memo.expect, _.size(keys));
-            console.log(memo.csv);
-            console.log(JSON.stringify(entry, undefined, 2));
-            throw new Error("Format error");
-        }
-
-        _.each(keys, function(k, i) {
-            let swap = _.get(entry, k, "");
-            if(k == 'savingTime')
-                memo.csv += moment(swap).toISOString();
-            else if(_.isInteger(swap))
-                memo.csv += swap;
-            else if(k == 'related') {
-                console.log(JSON.stringify(swap, undefined, 2))
-                console.log("related content!");
-            }
-            else {
-                swap = _.replace(swap, /"/g, '〃');
-                swap = _.replace(swap, /'/g, '’');
-                memo.csv +=  '"' + swap + '"';
-            }
-            if(!_.eq(i, _.size(keys) - 1))
-                memo.csv += ',';
-        });
-        memo.csv += "\n";
-        return memo;
-
-    }, { init: false, csv: "", expect: 0 });
-    return produced.csv;
-};
+        if (!_.eq(i, _.size(keys) - 1)) memo.csv += ',';
+      });
+      memo.csv += '\n';
+      return memo;
+    },
+    { init: false, csv: '', expect: 0 }
+  );
+  return produced.csv;
+}
 
 module.exports = {
-    produceCSVv1
+  produceCSVv1,
 };
