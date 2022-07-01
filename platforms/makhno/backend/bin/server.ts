dbe3ca6ba1c2d13341deb383f5614f77e935e17c
Claudio <claudio@syrianarchive.org>
2022-07-01 03:28:23 +0200

    chore(makhno): small renaming

--- platforms/makhno/backend/bin/server.ts
+++ platforms/makhno/backend/bin/server.ts
@@ -21,7 +21,7 @@ async function initialSanityChecks(): Promise<void> {
   /* security checks = is the password set and is not the default? (more checks might come) */
   security.checkKeyIsSet();
   await dbUtils.checkMongoWorks(true /* if true means that failure is fatal */);
-  appLogger('tiktok.tracking.exposed backend is operative!');
+  appLogger('Makhno backend is operative!');
 }
 
 async function start(): Promise<void> {
