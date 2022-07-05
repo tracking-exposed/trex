d50973311db2f94827007528b50d33c172e69131
Claudio Agosti <claudio@syrianarchive.org>
2022-07-05 15:39:41 +0200

    chore(tktrex): return proper error when the wrong experiment is invoked

--- platforms/tktrex/backend/routes/directives.js
+++ platforms/tktrex/backend/routes/directives.js
@@ -1,104 +1,60 @@
-const _ = require('lodash');
 const debug = require('debug')('routes:directives');
 const nconf = require('nconf');
-// const automo = require('../lib/automo');
-// const utils = require('../lib/utils');
 const experlib = require('../lib/experiments');
 const mongo3 = require('../lib/mongo3');
 
-// function reproducibleTypo(title) {
-//   const trimmedT = title.replace(/.$/, '').replace(/^./, '');
-//   return trimmedT; /*
-//   const stats = _.countBy(_.flatten(_.chunk(trimmedT)));
-//   let selection = null;
-//   _.each(_.reverse(stats), function(amount, letter) {
-//     if(!selection && amount === 1)
-//       selection = letter;
-//   });
-//   if(!selection)
-//     selection = _.last(stats).letter;
-
-//   injection = ' з ';
-//   const chunks = trimmedT.split(selection);
-//   return chunks.join(injection); */
-// }
-
-function searchesDirectory(query, counter) {
-  const url = 'https://tiktok.com/search?q=' + encodeURIComponent(query);
-  return {
-    url,
-    loadFor: 5500,
-    name: `search-for-${query}-${counter}`,
-    query,
-  };
-}
-
 async function post(req) {
-  /* remind self guardoni0 non fa registerDirective, codice mai chiamato sotto */
-  const directiveType = _.get(req.params, 'directiveType', '');
-  const directiveTypes = ['search'];
+  const directives = req.body;
 
-  if (directiveTypes.indexOf(directiveType) === -1) {
-    debug(
-      'Invalid directive type (%s), supported %j)',
-      directiveType,
-      directiveTypes
-    );
-    return { json: { error: true, message: 'Invalid directive type' } };
-  }
-
-  /* warning, this is different from YOUTUBE, it uses a list of
-   * queries instead of a CSV format parsed. */
-
-  const directives = req.body; // parsedCSV was the name from guardoni fmt
-
-  debug(
-    'Registering directive %s (%d urls)',
-    directiveType,
-    _.size(directives)
-  );
-
-  const feedback = await experlib.registerDirective(directives, directiveType);
+  const feedback = await experlib.registerDirective(directives);
   // this feedback is printed at terminal when --csv is used
   return { json: feedback };
 }
 
 async function get(req) {
   const experimentId = req.params.experimentId;
-  // const links = req.params.links;
-  debug('GET: should return directives for %s', experimentId);
+  debug('requested links for directive %s', experimentId);
   const expinfo = await experlib.pickDirective(experimentId);
 
   if (!expinfo) {
-    throw new Error(`Experiment ${experimentId} not found`);
+    debug(`Experiment ${experimentId} not found`);
+    return {
+      json: { error: true, message: 'Not found: experimentId not configured' },
+    };
   }
-  debug('Directive %O', expinfo);
-  // if(expinfo.directiveType === 'search')
+
   const links = expinfo.directives ?? expinfo.links ?? [];
-  debug('search directive type %s produced %d', experimentId, links.length);
+
+  if (!links.length) {
+    debug(
+      `Experiment ${experimentId} invalid format? has zero links to navigate on`
+    );
+    return {
+      json: {
+        error: true,
+        message: 'Invalid experiment, no links found',
+        details: [expinfo],
+      },
+    };
+  }
+
   return { json: links };
 }
 
 async function getPublic(req) {
-  const blackList = [
-    // 'b3d531eca62b2dc989926e0fe21b54ab988b7f3d',
-    // prod ids
-    'd75f9eaf465d2cd555de65eaf61a770c82d59451',
-    '37384a9b7dff26184cdea226ad5666ca8cbbf456',
-  ];
-
-  const filter = {
-    experimentId: {
-      $nin: blackList,
-    },
-  };
+  // everything would be public until we don't implement the visibility logic,
+  // which is not a problem of this API, but a registration default
+  // plus an admin-only API to switch visibility.
 
   const mongoc = await mongo3.clientConnect({ concurrency: 1 });
+  // TODO we've to implement paging in this API because it affect
+  // Guardoni-UX and, at the moment, we forcefully cap to 20 the max results
 
+  // TODO this API can also be cached with the same logic of statistics
   const publicDirectives = await mongo3.readLimit(
     mongoc,
     nconf.get('schema').directives,
-    filter,
+    {}, // TODO { visibility: 'public' },
     { when: -1 },
     20,
     0
@@ -112,7 +68,6 @@ async function getPublic(req) {
 }
 
 module.exports = {
-  searchesDirectory,
   post,
   get,
   getPublic,
