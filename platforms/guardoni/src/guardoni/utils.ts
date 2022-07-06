7363482c01ffc52540af4e0b19b21c7936868c14
ascariandrea <dev.ascariandrea@gmail.com>
2022-07-06 16:05:33 +0200

    fix(guardoni): add standard chrome path for macOS

--- platforms/guardoni/src/guardoni/utils.ts
+++ platforms/guardoni/src/guardoni/utils.ts
@@ -18,6 +18,7 @@ export const CHROME_PATHS = [
   'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
   'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
   '/Applications/Chromium.app/Contents/MacOS/Chromium',
+  '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
 ];
 
 // this function check for standard chrome executable path and
