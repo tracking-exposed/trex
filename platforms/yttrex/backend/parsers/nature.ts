3507b64883672a6fdefe5716985031db76ae8c62
ascariandrea <dev.ascariandrea@gmail.com>
2022-09-06 15:26:38 +0200

    fix(yttrex): await parser nature promise

--- platforms/yttrex/backend/parsers/nature.ts
+++ platforms/yttrex/backend/parsers/nature.ts
@@ -1,7 +1,7 @@
 import { ParserFn } from '@shared/providers/parser.provider';
 import { HTMLSource } from '../lib/parser/html';
 import processHome from './home';
-import parseVideo  from './video';
+import parseVideo from './video';
 import { processSearch } from './searches';
 import { Nature } from '../models/Nature';
 
@@ -25,10 +25,11 @@ const processNature = (type: Nature['type'], e: HTMLSource): any => {
  */
 export const nature: ParserFn<HTMLSource, Nature> = async (e) => {
   const type = e.html.nature.type ?? (e.html as any).type;
-  const nature = processNature(type, e);
+  const nature = await processNature(type, e);
   if (!nature) {
     throw new Error('No nature found for this entry.');
   }
+
   return {
     type,
     ...nature,
