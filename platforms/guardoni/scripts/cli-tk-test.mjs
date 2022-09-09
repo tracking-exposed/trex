25f56ebfa206e9d4a415c54a9d6586ba44b68118
ascariandrea <dev.ascariandrea@gmail.com>
2022-09-09 16:11:55 +0200

    chore(tktrex): cli tk test public key

--- platforms/guardoni/scripts/cli-tk-test.mjs
+++ platforms/guardoni/scripts/cli-tk-test.mjs
@@ -12,16 +12,15 @@ const version = await $`node -p -e "require('./package.json').version"`;
 try {
   const response = await fetch('http://localhost:14000/api/v0/health');
   const isRunning = await response.text();
-  if(isRunning !== 'OK') {
+  if (isRunning !== 'OK') {
     throw new Error();
   }
   console.log('\x1b[33mbackend running!\x1b[0m');
 } catch (e) {
-  console.log("backend not running? run:");
+  console.log('backend not running? run:');
   console.log('\x1b[33m$ yarn pm2 start platforms/ecosystem.config.js \x1b[0m');
   process.exit(1);
 }
-
 
 const profile = 'profile-test-99';
 
@@ -42,7 +41,6 @@ const cli = `./dist/guardoni-cli-${version.stdout.replace('\n', '')}-linux`;
 
 // eslint-disable-next-line no-void
 void (async function () {
-
   fs.removeSync(path.resolve(process.cwd(), 'profiles', profile));
 
   // register an experiment for search
@@ -51,23 +49,24 @@ void (async function () {
   const tk_search_experiment_id =
     tk_search_experiment_register_out.stdout.replace('experimentId: \t ', '');
 
-  await $`echo ${tk_search_experiment_id}`
+  await $`echo ${tk_search_experiment_id}`;
 
   // reject cookie modal
-  await $`${cli} ${cliFlags} tk-navigate --cookie-modal=reject --headless=false`
+  await $`${cli} ${cliFlags} tk-navigate --cookie-modal=reject --headless=false`;
 
   // # exec the experiment
   const tk_search_experiment_run_out =
     await $`(${cli} ${experimentCLIFlags} tk-experiment ${tk_search_experiment_id} |  grep 'publicKey:')`;
 
-  const tk_search_experiment_public_key =
-    tk_search_experiment_run_out.stdout.replace('publicKey: ', '');
+  const tk_search_experiment_public_key = tk_search_experiment_run_out.stdout
+    .replace('publicKey: \t', '')
+    .replace('\n', '');
 
   await $`echo ${tk_search_experiment_public_key}`;
 
   // check publicKey match when given as env variable
 
-  assert.strictEqual(tk_search_experiment_id, process.env.PUBLIC_KEY);
+  assert.strictEqual(tk_search_experiment_public_key, process.env.PUBLIC_KEY);
 
   await $`curl http://localhost:14000/api/v1/personal/${tk_search_experiment_public_key}/search/json`;
 
