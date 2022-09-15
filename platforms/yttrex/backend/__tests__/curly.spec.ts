84c58b55c3ea1f8d634865adecdd8b16e521a136
ascariandrea <dev.ascariandrea@gmail.com>
2022-09-15 16:48:14 +0200

    test(yttrex): use 'node-fetch' mock for curly spec test

--- platforms/yttrex/backend/__tests__/curly.spec.ts
+++ platforms/yttrex/backend/__tests__/curly.spec.ts
@@ -1,7 +1,18 @@
+import fetchMock from 'jest-fetch-mock';
 import { recentVideoFetch } from '../lib/curly';
 
+import initialData from './fixtures/ytInitialData.json';
+
 describe('The recentVideoFetch function', () => {
+  beforeAll(() => {
+    fetchMock.enableMocks();
+  });
   it('retrieves the videos from tracking-exposed', async () => {
+    fetchMock.mockResponseOnce(`
+      <script>
+        var ytInitialData = ${initialData};
+      </script>
+    `);
     const videos = await recentVideoFetch('UCZQ6RK_b9grwh1V9zdo3exA');
     expect(videos.length).toBeGreaterThanOrEqual(3);
   });
