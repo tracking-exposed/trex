65861e169faa7afee7c59e2649911c286c51cd5e
ascariandrea <dev.ascariandrea@gmail.com>
2022-09-22 10:42:37 +0200

    chore(workspace): shrink to zip strategy and TODOs for extension reviewer

--- scripts/install-from-shrinkzip.sh
+++ scripts/install-from-shrinkzip.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/bash
 
-set -x
+set -ex
 
 # This script is necessary to produce the .zip files to submit to mozilla store
 # Otherwise the repo is too big
 
-out=../trex-from-zip
+from=${1:-build/trex.zip}
+out=${2:-./build}
 
 mkdir -p $out
 
 # cp ./build/trex.zip $out/
 
-unzip -o -d $out build/trex.zip
+unzip -o -d $out $from
 
 cd $out || exit
 
 yarn install
 
-yarn tk:ext build
-yarn yt:ext build
+yarn tk:ext dist
+ls -l platforms/tktrex/extension/dist/*.zip
+yarn yt:ext dist
+ls -l platforms/yttrex/extension/dist/*.zip
 yarn ycai build
+ls -l platforms/ycai/studio/build/extension/*.zip
+
