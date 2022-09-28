b6283f696ad61a93d18c8deabdc0fae78990b011
ascariandrea <dev.ascariandrea@gmail.com>
2022-09-28 17:53:18 +0200

    build(workspace): upgrade release-it@^15

--- .release-it.js
+++ .release-it.js
@@ -2,21 +2,21 @@
 
 const conventionalCommitTypes = [
   // MINOR
-  { type: 'feat', release: 'minor' },
-  { type: 'feature', release: 'minor' },
+  { type: 'feat', release: 'minor', section: 'Features' },
+  { type: 'feature', release: 'minor', section: 'Features' },
   // PATCH
-  { type: 'fix', release: 'patch' },
-  { type: 'refactor', release: 'patch' },
-  { type: 'chore', release: 'patch' },
-  { type: 'ci', release: 'patch' },
-  { type: 'test', release: 'patch' },
-  { type: 'bugfix', release: 'patch' },
-  { type: 'hotfix', release: 'patch' },
-  { type: 'perf', release: 'patch' },
-  { type: 'improvement', release: 'patch' },
-  { type: 'revert', release: 'patch' },
-  { type: 'style', release: 'patch' },
-  { type: 'docs', release: 'patch' },
+  { type: 'fix', release: 'patch', section: 'Fixes' },
+  { type: 'refactor', release: 'patch', section: 'Fixes' },
+  { type: 'chore', release: 'patch', section: 'Fixes' },
+  { type: 'ci', release: 'patch', section: 'Fixes' },
+  { type: 'test', release: 'patch', section: 'Fixes' },
+  { type: 'bugfix', release: 'patch', section: 'Fixes' },
+  { type: 'hotfix', release: 'patch', section: 'Fixes' },
+  { type: 'perf', release: 'patch', section: 'Fixes' },
+  { type: 'improvement', release: 'patch', section: 'Fixes' },
+  { type: 'revert', release: 'patch', section: 'Fixes' },
+  { type: 'style', release: 'patch', section: 'Fixes' },
+  { type: 'docs', release: 'patch', section: 'Docs' },
   // NO RELEASE
   { type: 'ci', release: false },
   { type: 'build', release: false },
@@ -45,8 +45,13 @@ module.exports = {
         name: 'conventionalcommits',
         types: conventionalCommitTypes,
       },
+      // not really useful until this PR is merged
+      // https://github.com/conventional-changelog/conventional-changelog/pull/908
+      // writerOpts: {
+      //   groupBy: 'scope',
+      // },
     },
-    'release-it-yarn-workspaces': {
+    '@release-it-plugins/workspaces': {
       publish: false,
       skipChecks: true,
     },
