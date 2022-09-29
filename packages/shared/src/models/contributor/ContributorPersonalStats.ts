1ab399449b93f8b28c02ed27571ee8b8574d76a4
ascariandrea <dev.ascariandrea@gmail.com>
2022-09-29 10:29:47 +0200

    fix(taboule): tag codec for supporter in personal stats request

--- packages/shared/src/models/contributor/ContributorPersonalStats.ts
+++ packages/shared/src/models/contributor/ContributorPersonalStats.ts
@@ -40,37 +40,49 @@ export type SearchMetadata = t.TypeOf<typeof SearchMetadata>;
 
 export const ContributorPersonalStats = t.strict(
   {
-    supporter: t.strict({
-      _id: t.string,
-      publicKey: t.string,
-      creationTime: t.string,
-      p: t.string,
-      lastActivity: t.string,
-      version: t.string,
-      tag: t.strict({
-        id: t.string,
-        name: t.string,
-        accessibility: t.string,
-        lastAccess: t.string,
-        description: t.string,
-        // _id: t.string
-      }),
-      hereSince: t.string,
-    }),
+    supporter: t.strict(
+      {
+        _id: t.string,
+        publicKey: t.string,
+        creationTime: t.string,
+        p: t.string,
+        lastActivity: t.string,
+        version: t.string,
+        tag: t.union([
+          t.undefined,
+          t.strict({
+            id: t.string,
+            name: t.string,
+            accessibility: t.string,
+            lastAccess: t.string,
+            description: t.string,
+            // _id: t.string
+          }),
+        ]),
+        hereSince: t.string,
+      },
+      'Supporter'
+    ),
     videos: t.array(VideoMetadata),
     homes: t.array(HomeMetadata),
     searches: t.array(SearchMetadata),
     ads: t.array(t.any),
-    stats: t.strict({
-      home: t.number,
-      video: t.number,
-      search: t.number,
-    }),
-    request: t.strict({
-      amount: t.number,
-      skip: t.number,
-      when: t.string,
-    }),
+    stats: t.strict(
+      {
+        home: t.number,
+        video: t.number,
+        search: t.number,
+      },
+      'Stats'
+    ),
+    request: t.strict(
+      {
+        amount: t.number,
+        skip: t.number,
+        when: t.string,
+      },
+      'Request'
+    ),
   },
   'ContributorPersonalStats'
 );
