8e742fa6e83bd86673e807e66dd1eac15c18822d
ascariandrea <dev.ascariandrea@gmail.com>
2022-10-11 15:17:01 +0200

    test(tk:backend): personal csv results for search and native nature

--- platforms/tktrex/shared/src/arbitraries/Metadata.arb.ts
+++ platforms/tktrex/shared/src/arbitraries/Metadata.arb.ts
@@ -4,6 +4,8 @@ import {
   ForYouVideoMetadata,
   FollowingVideoMetadata,
   MetadataBase,
+  SearchMetadataResult,
+  NativeMetadata,
 } from '../models/Metadata';
 import { propsOmitType } from '@shared/utils/arbitrary.utils';
 import { fc } from '@shared/test';
@@ -11,6 +13,49 @@ import { subDays } from 'date-fns';
 import * as t from 'io-ts';
 
 const metadataBaseProps = propsOmitType(MetadataBase, ['id', 'savingTime']);
+
+/**
+ * Native metadata arbitrary
+ */
+
+const nativeMetadataProps = propsOmitType(NativeMetadata.types[3], []);
+
+export const NativeMetadataArb: fc.Arbitrary<NativeMetadata> = getArbitrary(
+  t.type({
+    ...metadataBaseProps,
+    ...NativeMetadata.types[1].type.props,
+    ...nativeMetadataProps,
+  })
+).map(({ videoId, authorId, ...n }) => {
+  return {
+    ...n,
+    videoId,
+    authorId,
+    id: fc.sample(fc.uuid(), 1)[0],
+    nature: { type: 'native', videoId, authorId },
+    savingTime: fc.sample(fc.date(), 1)[0].toISOString(),
+  };
+});
+
+/**
+ * Search metadata results
+ */
+
+const searchMetadataResultProps = propsOmitType(SearchMetadataResult, [
+  // 'linked',
+  // 'video',
+]);
+export const SearchMetadataResultArb = getArbitrary(
+  t.strict({
+    ...searchMetadataResultProps,
+  })
+).map((s) => ({
+  ...s,
+  publishingDate: fc.sample(
+    fc.date().map((d) => d.toISOString()),
+    1
+  )[0],
+}));
 
 /**
  * SearchMetadata arbitrary
@@ -22,16 +67,30 @@ const searchMetadataProps = propsOmitType(SearchMetadata.types[3], [
   'thumbnails',
 ]);
 
-export const SearchMetaDataArb = getArbitrary(
-  t.intersection([t.type(metadataBaseProps), t.type(searchMetadataProps)]),
-).map((ad) => ({
-  ...ad,
-  id: fc.sample(fc.uuid(), 1)[0],
-  savingTime: subDays(new Date(), fc.sample(fc.nat(), 1)[0]),
-  clientTime: fc.sample(fc.date(), 1)[0],
-  results: [],
-  thumbnails: [],
-}));
+export const SearchMetaDataArb = (opts: {
+  results: number;
+}): fc.Arbitrary<SearchMetadata> =>
+  getArbitrary(
+    t.intersection([t.type(metadataBaseProps), t.type(searchMetadataProps)])
+  ).map((ad) => {
+    const nature = {
+      type: 'search' as const,
+      query: fc.sample(fc.uuid(), 1)[0],
+    };
+    return {
+      ...ad,
+      nature,
+      ...nature,
+      id: fc.sample(fc.uuid(), 1)[0],
+      savingTime: subDays(
+        new Date(),
+        fc.sample(fc.oneof(fc.constant(5), fc.constant(10)), 1)[0]
+      ).toISOString(),
+      clientTime: fc.sample(fc.date(), 1)[0],
+      results: fc.sample(SearchMetadataResultArb, opts.results),
+      thumbnails: [],
+    };
+  });
 
 /**
  * ForYouMetadata arbitrary
@@ -40,7 +99,7 @@ export const SearchMetaDataArb = getArbitrary(
 const forYouMetadataProps = propsOmitType(ForYouVideoMetadata.types[2], []);
 
 export const ForYouVideoMetaDataArb = getArbitrary(
-  t.intersection([t.type(metadataBaseProps), t.type(forYouMetadataProps)]),
+  t.intersection([t.type(metadataBaseProps), t.type(forYouMetadataProps)])
 ).map((ad) => ({
   ...ad,
   id: fc.sample(fc.uuid(), 1)[0],
@@ -54,14 +113,14 @@ export const ForYouVideoMetaDataArb = getArbitrary(
  **/
 const followingVideoMetadataProps = propsOmitType(
   FollowingVideoMetadata.types[1],
-  [],
+  []
 );
 
 export const FollowingVideoMetaDataArb = getArbitrary(
   t.intersection([
     t.type(metadataBaseProps),
     t.type(followingVideoMetadataProps),
-  ]),
+  ])
 ).map((ad) => ({
   ...ad,
   id: fc.sample(fc.uuid(), 1)[0],
@@ -70,7 +129,8 @@ export const FollowingVideoMetaDataArb = getArbitrary(
 }));
 
 export const MetadataArb = fc.oneof(
-  SearchMetaDataArb,
+  SearchMetaDataArb({ results: 10 }),
   ForYouVideoMetaDataArb,
   FollowingVideoMetaDataArb,
+  NativeMetadataArb
 );
