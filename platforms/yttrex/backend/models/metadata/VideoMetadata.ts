e500dcf52330e17326c138aca22c2e21b2dee74f
ascariandrea <dev.ascariandrea@gmail.com>
2022-10-12 11:14:27 +0200

    fix(yt:backend): defined VideoMetadataDB model

---
+++ platforms/yttrex/backend/models/metadata/VideoMetadata.ts
@@ -0,0 +1,25 @@
+import { ParsedInfo, VideoMetadata } from '@yttrex/shared/models/Metadata';
+import * as t from 'io-ts';
+import { date } from 'io-ts-types/lib/date';
+
+const { supporter, ...videoMetadataProps } = VideoMetadata.type.props;
+export const VideoMetadataDB = t.type(
+  {
+    ...videoMetadataProps,
+    publicKey: t.string,
+    savingTime: date,
+    clientTime: date,
+    publicationTime: date,
+    related: t.array(
+      t.strict(
+        {
+          ...ParsedInfo.type.props,
+          publicationTime: t.union([date, t.null, t.undefined]),
+        },
+        'VideoMetadataRelated'
+      )
+    ),
+  },
+  'VideoMetadataDB'
+);
+export type VideoMetadataDB = t.TypeOf<typeof VideoMetadataDB>;
