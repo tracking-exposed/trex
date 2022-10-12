15b9e335073121d0889fe2f745d7631cda3f3f80
ascariandrea <dev.ascariandrea@gmail.com>
2022-10-12 11:32:12 +0200

    fix(yt:backend): defined SearchMetadataDB model

---
+++ platforms/yttrex/backend/models/metadata/SearchMetadata.ts
@@ -0,0 +1,16 @@
+import { propsOmit } from '@shared/utils/arbitrary.utils';
+import { SearchMetadata } from '@yttrex/shared/models/Metadata';
+import * as t from 'io-ts';
+import { date } from 'io-ts-types';
+
+const searchMetadataProps = propsOmit(SearchMetadata, ['supporter']);
+export const SearchMetadataDB = t.strict(
+  {
+    ...searchMetadataProps,
+    savingTime: date,
+    clientTime: date,
+  },
+  'SearchMetadata'
+);
+
+export type SearchMetadataDB = t.TypeOf<typeof SearchMetadataDB>;
