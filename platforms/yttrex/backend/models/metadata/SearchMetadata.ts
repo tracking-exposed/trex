9fc503bc2d03c65c66fe297e5ebb9aee2e68de93
ascariandrea <dev.ascariandrea@gmail.com>
2022-10-13 16:32:28 +0200

    chore(yt:backend): fix io-ts-types granular imports to prevent missing 'newtype-ts' dep

--- platforms/yttrex/backend/models/metadata/SearchMetadata.ts
+++ platforms/yttrex/backend/models/metadata/SearchMetadata.ts
@@ -1,7 +1,7 @@
 import { propsOmit } from '@shared/utils/arbitrary.utils';
 import { SearchMetadata } from '@yttrex/shared/models/Metadata';
 import * as t from 'io-ts';
-import { date } from 'io-ts-types';
+import { date } from 'io-ts-types/lib/date';
 
 const searchMetadataProps = propsOmit(SearchMetadata, ['supporter']);
 export const SearchMetadataDB = t.strict(
