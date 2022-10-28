c4757184cc15aa5cc7db20e39e204ac20a81a9bf
Andrea Ascari <dev.ascariandrea@gmail.com>
2022-10-28 09:30:03 +0200

    fix(yt:backend): defined /v0/health endpoint (#766)

--- platforms/tktrex/backend/bin/app.ts
+++ platforms/tktrex/backend/bin/app.ts
@@ -1,13 +1,16 @@
 import { MakeAppContext } from '@shared/backend/app';
 import express from 'express';
 import apiList from '../lib/api';
-import D from 'debug';
 import cors from 'cors';
 import path from 'path';
 import bodyParser from 'body-parser';
 import { routeHandleMiddleware } from '@shared/backend/utils/routeHandlerMiddleware';
+import { makeBackend } from '@shared/backend';
+import { RouteContext } from '@shared/backend/types';
+import * as mongo3 from '@shared/providers/mongo.provider';
+import { GetLogger } from '@shared/logger';
 
-export const appLogger = D('tktrex');
+export const appLogger = GetLogger('tktrex');
 
 const iowrapper = routeHandleMiddleware(apiList);
 
@@ -22,84 +25,89 @@ export const makeApp = async (
     bodyParser.urlencoded({ limit: '10mb', extended: true, parameterLimit: 10 })
   );
 
-  /* this API is v0 as it is platform neutral. it might be shared among
-   * all the trex backends, and should return info on system health, echo OK
-   * if the system is OK, and the git log of the code running */
-  app.get('/api/v0/health', function (req, res) {
-    res.send('OK');
-    res.status(200);
-  });
+  // get a router instance from express
+  const router = express.Router();
+
+  const routeCtx: RouteContext = {
+    logger: appLogger,
+    db: {
+      ...mongo3,
+      mongo: ctx.mongo,
+    },
+  };
+
+  const apiRouter = makeBackend(routeCtx, express.Router());
 
   /* This is the API meant to receive data donation */
-  app.post('/api/v2/events', iowrapper('processEvents'));
-  app.post('/api/v2/apiEvents', iowrapper('processAPIEvents'));
-  app.post('/api/v2/handshake', iowrapper('handshake'));
+  apiRouter.post('/v2/events', iowrapper('processEvents'));
+  apiRouter.post('/v2/apiEvents', iowrapper('processAPIEvents'));
+  apiRouter.post('/v2/handshake', iowrapper('handshake'));
 
-  app.get('/api/v2/recent', iowrapper('getRecent'));
+  apiRouter.get('/v2/recent', iowrapper('getRecent'));
 
-  app.get('/api/v2/statistics/:name/:unit/:amount', iowrapper('getStatistics'));
+  apiRouter.get('/v2/statistics/:name/:unit/:amount', iowrapper('getStatistics'));
 
   /* debug API */
-  app.get('/api/v2/debug/html/:htmlId', iowrapper('unitById'));
-  app.get('/api/v1/mirror/:key', iowrapper('getMirror'));
+  apiRouter.get('/v2/debug/html/:htmlId', iowrapper('unitById'));
+  apiRouter.get('/v1/mirror/:key', iowrapper('getMirror'));
 
   /* monitor for admin */
-  app.get('/api/v2/monitor/:minutes?', iowrapper('getMonitor'));
+  apiRouter.get('/v2/monitor/:minutes?', iowrapper('getMonitor'));
 
   /* special function for researchers */
-  app.get(
-    '/api/v2/research/:publicKeyList/:what/csv',
+  apiRouter.get(
+    '/v2/research/:publicKeyList/:what/csv',
     iowrapper('getResearcherData')
   );
 
   /* experiments API: at the moment not implemented for tiktok, only copied by yttrex */
 
-  app.get('/api/v2/guardoni/list', iowrapper('getAllExperiments'));
+  apiRouter.get('/v2/guardoni/list', iowrapper('getAllExperiments'));
 
-  app.get('/api/v2/directives/public', iowrapper('getPublicDirectives'));
+  apiRouter.get('/api/v2/directives/public', iowrapper('getPublicDirectives'));
 
-  app.post('/api/v2/directives', iowrapper('postDirective'));
-  app.get('/api/v2/directives/:experimentId', iowrapper('fetchDirective'));
-  app.post('/api/v2/handshake', iowrapper('experimentChannel3'));
-  app.delete('/api/v2/experiment/:testTime', iowrapper('concludeExperiment3'));
-  app.get('/api/v2/experiment/:experimentId/json', iowrapper('experimentJSON'));
+  apiRouter.post('/v2/directives', iowrapper('postDirective'));
+  apiRouter.get('/v2/directives/:experimentId', iowrapper('fetchDirective'));
+  apiRouter.post('/v2/handshake', iowrapper('experimentChannel3'));
+  apiRouter.delete('/v2/experiment/:testTime', iowrapper('concludeExperiment3'));
+  apiRouter.get('/v2/experiment/:experimentId/json', iowrapper('experimentJSON'));
   // TODO unify the two API below and the one above
-  app.get(
-    '/api/v2/experiment/:experimentId/csv/:type',
+  apiRouter.get(
+    '/v2/experiment/:experimentId/csv/:type',
     iowrapper('experimentCSV')
   );
-  app.get('/api/v2/experiment/:experimentId/dot', iowrapper('experimentDOT'));
+  apiRouter.get('/v2/experiment/:experimentId/dot', iowrapper('experimentDOT'));
 
   /* subscription email */
-  app.post('/api/v1/registerEmail2', iowrapper('registerEmail2'));
-  app.get('/api/v1/listEmails/:key', iowrapper('listEmails'));
+  apiRouter.post('/v1/registerEmail2', iowrapper('registerEmail2'));
+  apiRouter.get('/v1/listEmails/:key', iowrapper('listEmails'));
   /* ============== Documented only the API below ============== */
 
   /* TODO the JSON was v1 and should be fixed in site, the what should be a query string, should be timed params */
-  app.get(
-    '/api/v2/personal/:publicKey/experiments/:experimentId/:format',
+  apiRouter.get(
+    '/v2/personal/:publicKey/experiments/:experimentId/:format',
     iowrapper('getPersonalByExperimentId')
   );
 
-  app.get(
-    '/api/v[1-2]/personal/:publicKey/:what/json',
+  apiRouter.get(
+    '/v[1-2]/personal/:publicKey/:what/json',
     iowrapper('getPersonal')
   );
-  app.get('/api/v2/personal/:publicKey/:what/csv', iowrapper('getPersonalCSV'));
+  apiRouter.get('/v2/personal/:publicKey/:what/csv', iowrapper('getPersonalCSV'));
 
   /* implemented for DMI winter school, supported in Taboule, trimming in progress */
-  app.get('/api/v2/public/searches', iowrapper('getSearches'));
-  app.get(
-    '/api/v2/public/query/:string/:format',
+  apiRouter.get('/v2/public/searches', iowrapper('getSearches'));
+  apiRouter.get(
+    '/v2/public/query/:string/:format',
     iowrapper('getSearchByQuery')
   );
-  app.get('/api/v2/public/queries/list', iowrapper('getQueryList'));
-  app.get('/api/v2/metadata', iowrapper('listMetadata'));
+  apiRouter.get('/v2/public/queries/list', iowrapper('getQueryList'));
+  apiRouter.get('/v2/metadata', iowrapper('listMetadata'));
 
   /* --------------- end of documented APIs -------------------- */
 
   /* quick experiment to return static images: nginx might do a better job */
-  app.get('/api/v0/images/:subd/:fname', (req, res) => {
+  apiRouter.get('/v0/images/:subd/:fname', (req, res) => {
     res.sendFile(
       path.join(
         process.cwd(),
@@ -111,9 +119,13 @@ export const makeApp = async (
     );
   });
 
+  router.use('/api', apiRouter)
+
+  app.use(router);
+
   /* Capture All 404 errors */
   app.get('*', async (req, res) => {
-    appLogger('URL not handled: %s', req.url);
+    appLogger.debug('URL not handled: %s', req.url);
     res.status(404);
     res.send('URL not found');
   });
