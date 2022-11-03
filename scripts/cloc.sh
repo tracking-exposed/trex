4792d089d4a535fbf1f03f657b96bb01daef9136
ascariandrea <dev.ascariandrea@gmail.com>
2022-11-03 17:32:44 +0100

    chore(workspace): cloc script grouping by file and by lang

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -1,41 +1,42 @@
 #!/usr/bin/env bash
 
+export flags="--exclude-dir node_modules,build,__tests__,coverage --by-file-by-lang"
 # Shared
-export shared=./packages/shared
+export shared=./packages/shared/src
 echo $shared
-cloc $shared --exclude-dir node_modules
+cloc $shared $flags
 
 # Taboule
-export taboule=./packages/taboule
+export taboule=./packages/taboule/src
 echo $taboule
-cloc $taboule --exclude-dir node_modules
+cloc $taboule $flags
 
 # TK TrEx Shared
-export tktrex_shared=./platforms/tktrex/shared
+export tktrex_shared=./platforms/tktrex/shared/src
 echo $tktrex_shared
-cloc $tktrex_shared --exclude-dir node_modules
+cloc $tktrex_shared $flags
 
 # TK TrEx Ext
-export tktrex_ext=./platforms/tktrex/extension
+export tktrex_ext=./platforms/tktrex/extension/src
 echo $tktrex_ext
-cloc $tktrex_ext --exclude-dir node_modules
+cloc $tktrex_ext $flags
 
 # TK TrEx Backend
 export tktrex_backend=./platforms/tktrex/backend
 echo $tktrex_backend
-cloc $tktrex_backend --exclude-dir node_modules
+cloc $tktrex_backend $flags
 
 # YT TrEx Shared
 export yttrex_shared=./platforms/yttrex/shared/src
 echo $yttrex_shared
-cloc $yttrex_shared --exclude-dir node_modules
+cloc $yttrex_shared $flags
 
 # YT TrEx Ext
-export yttrex_ext=./platforms/yttrex/extension
+export yttrex_ext=./platforms/yttrex/extension/src
 echo $yttrex_ext
-cloc $yttrex_ext --exclude-dir node_modules
+cloc $yttrex_ext $flags
 
 # YT TrEx Backend
 export yttrex_backend=./platforms/yttrex/backend
 echo $yttrex_backend
-cloc $yttrex_backend --exclude-dir node_modules
+cloc $yttrex_backend $flags
