5978fad6aba463f7f23cea778d90b08b9cfd96c7
Andrea Ascari <andrea@tracking.exposed>
2022-11-22 15:03:58 +0100

    feat(shared): error "snackbar" component (#798)
    Co-authored-by: koke <rekoke92@gmail.com>

---
+++ packages/shared/src/components/ErrorModal/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReportProblemRoundedIcon from '@mui/icons-material/ReportProblemRounded';
+import clsx from 'clsx';
+import './errorModal.css';
+import { Box, Typography } from '@material-ui/core';
+import CloseIcon from '@mui/icons-material/Close';
+
+export interface ErrorModalProps {
+  name: string;
+  message: string;
+  details: string[];
+  position?: string[];
+  onClick: (action: string) => void;
+}
+
+const ErrorModal = (props: ErrorModalProps): JSX.Element => {
+  const { name, message, details, position, onClick } = props;
+  const [detailsShown, setDetailsShown] = React.useState(false);
+
+  const handleDetailsClick = (): void => {
+    setDetailsShown((prev) => !prev);
+  };
+  const handleDismiss = (): void => {
+    onClick('!');
+  };
+
+  return (
+    <Box className={`ErrorModal ${position}`}>
+      <div className="ErrorModal__container">
+        <div
+          className="ErrorModal__container__content"
+          onClick={handleDetailsClick}
+        >
+          <ReportProblemRoundedIcon />
+          <Typography className="ErrorModal__container__content__message">
+            {name}: {message}
+          </Typography>
+        </div>
+        <div className="ErrorModal__container__details">
+          <div
+            className={clsx('ErrorModal__container__details__box', {
+              '-showBox': detailsShown,
+            })}
+          >
+            <ul className="ErrorModal__container__details__box__detail">
+              {details.map((d) => (
+                <li key={d}>
+                  <Typography>{d}</Typography>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <button className="ErrorModal__close" onClick={handleDismiss}>
+          <CloseIcon />
+        </button>
+      </div>
+    </Box>
+  );
+};
+
+export default ErrorModal;
